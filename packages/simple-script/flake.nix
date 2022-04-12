43f156ecf0a8922aaa7d128d9c46f9abbdf34192
Simon W. Jackson <hello@simonwjackson.io>
2022-04-12 05:12:33 +0000

    refactor: move script into packages

---
+++ packages/simple-script/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A best script!";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        my-name = "my-script";
+        my-buildInputs = with pkgs; [
+          unzip
+          p7zip
+        ];
+        my-script = (pkgs.writeScriptBin my-name (builtins.readFile ./my-script.sh)).overrideAttrs (old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
+      in
+      rec {
+        defaultPackage = packages.my-script;
+
+        packages.my-script = pkgs.symlinkJoin {
+          name = my-name;
+          paths = [ my-script ] ++ my-buildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
+        };
+      }
+    );
+}
