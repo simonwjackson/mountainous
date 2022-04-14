bc4cff4d743f2b0349a7db7db97cd51ccee946d4
Simon W. Jackson <hello@simonwjackson.io>
2022-04-14 03:24:01 +0000

    refactor: modular polybar ini

--- users/simonwjackson/desktop/polybar/default.nix
+++ users/simonwjackson/desktop/polybar/default.nix
@@ -8,6 +8,12 @@
   services.polybar = {
     enable = true;
     extraConfig = builtins.readFile ./polybar.ini
+      + builtins.readFile ./modules/battery.ini
+      + builtins.readFile ./modules/cpu.ini
+      + builtins.readFile ./modules/filesystem.ini
+      + builtins.readFile ./modules/memory.ini
+      + builtins.readFile ./modules/pulseaudio.ini
+      + builtins.readFile ./modules/time.ini
       + import ./modules/network { inherit pkgs config; }
       + import ./modules/check-reddit { inherit pkgs config; }
     ;
@@ -15,20 +21,5 @@
     script = ''
       polybar top &
     '';
-
-    settings = {
-      "bar/top" = {
-        monitor = "\${env:MONITOR:eDP-1}";
-        width = "100%";
-        height = "3%";
-        radius = 0;
-        modules-left = "";
-        modules-center = "";
-        modules-right = "battery check-reddit network time";
-        font-0 = "NotoSansMono Nerd Font:pixelsize=20;5";
-        font-1 = "NotoSansMono Nerd Font:pixelsize=18;4";
-        font-2 = "NotoSansMono Nerd Font:pixelsize=15;4";
-      };
-    };
   };
 }
