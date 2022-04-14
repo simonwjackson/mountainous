1aa32f1dd96620f79c8f7aabb0a4114611984c27
Simon W. Jackson <hello@simonwjackson.io>
2022-04-14 16:58:25 +0000

    feat: add wireguard-client role

---
+++ modules/wireguard-client.nix
@@ -0,0 +1,22 @@
+{ config, pkgs }:
+
+{
+  networking.wireguard.interfaces = {
+    mtn = {
+      listenPort = 51820;
+
+      peers = [
+        # rakku
+        {
+          publicKey = "z5ymEx3zp7cEt8KkE7nO3TsqWJBOc3CycmwB171WYXU=";
+          allowedIPs = [
+            "192.18.1.0/24"
+            "192.18.2.0/24"
+          ];
+          endpoint = "45.20.193.255:56789";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+}
