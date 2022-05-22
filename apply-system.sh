75ed0dfc9b9939b304af9f911b128a7da3837454
Simon W. Jackson <hello@simonwjackson.io>
2022-05-22 17:53:37 +0000

    fix: update flake

--- apply-system.sh
+++ apply-system.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-if [ -n "$(git status --porcelain)" ]; then
-  echo "Commit your changes"
-  exit 1
-else
+# if [ -n "$(git status --porcelain)" ]; then
+#   echo "Commit your changes"
+#   exit 1
+# else
   sudo nixos-rebuild \
     -v switch \
     --profile-name "$(git log -1 --pretty=%B | sed "s/[^[:alnum:]-]/-/g")" \
     --flake '.#'
-fi
+# fi
--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
@@ -11,6 +12,7 @@
   outputs = { home-manager, nixpkgs, nixos-hardware, ... }:
     let
       system = "x86_64-linux";
+      #system = "aarch64-linux";
 
       pkgs = import nixpkgs {
         inherit system;
@@ -26,7 +28,8 @@
     in
     {
       homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-        inherit system pkgs username;
+        inherit pkgs username system;
+        # system = "aarch64-linux";
 
         configuration = import ./users/${username};
         homeDirectory = "/home/${username}";
@@ -42,6 +45,14 @@
             nixos-hardware.nixosModules.microsoft-surface
           ];
         };
+
+        # adama = lib.nixosSystem {
+        #  # system = "aarch64-linux";
+
+        #   modules = [
+        #     ./system/adama.nix
+        #   ];
+        # };
 
         nixos = lib.nixosSystem {
           inherit system;
