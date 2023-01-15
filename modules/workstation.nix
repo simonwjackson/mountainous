94d2fea5fc9c55058b37601fb6057c81024309b7
Simon W. Jackson <hello@simonwjackson.io>
2023-01-15 13:50:49 -0600

    feat: switch to pipewire

--- modules/workstation.nix
+++ modules/workstation.nix
@@ -34,10 +34,17 @@
   # Required when building a custom desktop env
   programs.dconf.enable = true;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  nixpkgs.config.pulseaudio = true;
+  hardware.pulseaudio.enable = false;
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    # jack.enable = true;
+  };
 
   environment.variables.BROWSER = "firefox";
 
