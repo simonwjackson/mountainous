ab6ecdcb15b585405409b457ca2f0134426ca9bf
Simon W. Jackson <hello@simonwjackson.io>
2023-03-28 11:52:14 -0500

    feat: switch to awesome

--- users/simonwjackson/desktop/default.nix
+++ users/simonwjackson/desktop/default.nix
@@ -12,6 +12,11 @@
     };
 
     file = {
+      "./.local/bin" = {
+        source = ../../../modules/cmd_p;
+        recursive = true;
+      };
+
       "./.config/tridactyl/tridactylrc" = {
         source = ./tridactylrc;
       };
@@ -29,11 +34,14 @@
       wmctrl
       xorg.xkill
       tridactyl-native
-      pulseaudio-ctl
+      pamixer
+      rofi
     ];
   };
 
-  xsession.windowManager.awesome.enable = true;
+  xsession.windowManager.awesome = {
+    enable = true;
+  };
 
   xsession = {
     enable = true;
@@ -84,50 +92,20 @@
 
   home.file.".config/kitty/kitty.base.conf".source = config.lib.file.mkOutOfStoreSymlink ./kitty/kitty.conf;
 
-  services.picom =
-    {
-      enable = true;
-      # vSync = true;
-      # backend = "xr_glx_hybrid";
-      # backend = "glx";
-      extraArgs = [ "--experimental-backend" ];
-      settings = import ./picom/picom.nix;
-      wintypes =
-        {
-          tooltip = {
-            fade = true;
-            shadow = true;
-            opacity = 0.95;
-            focus = true;
-            full-shadow = false;
-          };
-          dock = {
-            shadow = false;
-            clip-shadow-above = true;
-          };
-          dnd = {
-            shadow = false;
-          };
-          popup_menu =
-            {
-              opacity = 0.9;
-              full-shadow = false;
-            };
-          dropdown_menu = {
-            opacity = 0.9;
-            full-shadow = false;
-          };
-        };
+  services.picom = {
+    enable = true;
+    extraArgs = [ "--experimental-backend" ];
+    settings = import ./picom/picom.nix;
 
-      package = pkgs.picom.overrideAttrs (o: {
-        src = pkgs.fetchFromGitHub {
-          repo = "picom";
-          owner = "jonaburg";
-          rev = "e3c19cd7d1108d114552267f302548c113278d45";
-          sha256 = "4voCAYd0fzJHQjJo4x3RoWz5l3JJbRvgIXn1Kg6nz6Y=";
-        };
-      });
-    };
+    package = pkgs.picom.overrideAttrs (o: {
+      src = pkgs.fetchFromGitHub {
+        repo = "picom";
+        owner = "jonaburg";
+        rev = "e3c19cd7d1108d114552267f302548c113278d45";
+        sha256 = "4voCAYd0fzJHQjJo4x3RoWz5l3JJbRvgIXn1Kg6nz6Y=";
+      };
+    });
+  };
 
   services.sxhkd = {
     enable = true;
@@ -138,14 +116,13 @@
   programs.firefox = {
     enable = true;
 
-    package = pkgs.firefox.override
-      {
-        # See nixpkgs' firefox/wrapper.nix to check which options you can use
-        cfg = {
-          # Tridactyl native connector
-          enableTridactylNative = true;
-        };
+    package = pkgs.firefox-esr.override {
+      # See nixpkgs' firefox/wrapper.nix to check which options you can use
+      cfg = {
+        # Tridactyl native connector
+        enableTridactylNative = true;
       };
+    };
 
     profiles.simonwjackson = {
       isDefault = true;
@@ -154,14 +131,16 @@
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "browser.tabs.closeWindowWithLastTab" = true;
         "layout.css.prefers-color-scheme.content-override" = 2;
+        "xpinstall.signatures.required" = false;
+        "extensions.langpacks.signatures.required" = false;
       };
 
       userChrome = ''
         /* Hide back/forward buttons */
-        /* #back-button, #forward-button { display:none!important; }
-        */
+        #back-button, #forward-button, #home-button { display:none!important; }
+        
         /* Hide tab bar */
-        /* #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar > .toolbar-items {
+        #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar > .toolbar-items {
           opacity: 0;
           pointer-events: none;
         }
@@ -173,7 +152,6 @@
         #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar .titlebar-spacer {
           border-inline-end: none;
         }
-        */
       '';
 
       userContent = ''
