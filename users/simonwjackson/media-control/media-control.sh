4b45641f1f32554846dc99751fb2e96fbaef9f0e
Simon W. Jackson <hello@simonwjackson.io>
2023-04-11 18:49:40 -0500

    :notes: feat(media-control): Add media control module for NixOS
    This commit introduces a new media control module for NixOS, which allows users to control audio playback for various applications using media keys.
    
    Changes include:
    - Add `default.nix` for the media control module configuration
    - Add `media-control.sh` script to handle media key commands
    - Add `sxhkdrc` configuration file for binding media keys to the script
    
    This feature enhances the user experience by providing a unified way to control audio playback across different applications.

---
+++ users/simonwjackson/media-control/media-control.sh
@@ -0,0 +1,52 @@
+# Check if an application is playing audio
+is_playing_audio() {
+  pactl list sink-inputs | grep -q "application.name.*=.*$1"
+}
+
+# Send command to mpc
+send_mpc_command() {
+  case "$1" in
+    "XF86AudioPlay")
+      mpc toggle
+      ;;
+    "XF86AudioNext")
+      mpc next
+      ;;
+    "XF86AudioPrev")
+      mpc prev
+      ;;
+    esac
+}
+
+# Send command to mpv using JSON IPC
+send_mpv_command() {
+  case "$1" in
+    "XF86AudioPlay")
+      echo '{ "command": ["cycle", "pause"] }' | socat - UNIX-CONNECT:"$MPV_SOCKET"
+      ;;
+    "XF86AudioNext")
+      echo '{ "command": ["playlist-next"] }' | socat - UNIX-CONNECT:"$MPV_SOCKET"
+      ;;
+    "XF86AudioPrev")
+      echo '{ "command": ["playlist-prev"] }' | socat - UNIX-CONNECT:"$MPV_SOCKET"
+      ;;
+    esac
+}
+
+# Send keypress to the application
+send_keypress() {
+  WID=$(xdotool search --class "$1" | head -n 1)
+    xdotool key --window "$WID" "$2"
+}
+
+# Check applications and send keypresses
+if is_playing_audio "Music Player Daemon"; then
+send_mpc_command "$1"
+elif is_playing_audio "mpv"; then
+send_mpv_command "$1"
+elif is_playing_audio "Firefox"; then
+echo "Firefox"
+send_keypress "firefox" "$1"
+else
+echo "No matching application found playing audio."
+fi
