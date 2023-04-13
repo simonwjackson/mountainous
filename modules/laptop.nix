4b50b3a1ecd50907297925e13863fa8ff217134d
Simon W. Jackson <hello@simonwjackson.io>
2023-04-12 22:13:44 -0500

    :hammer: Disable touchpad support in laptop.nix
    This commit comments out the touchpad support configuration in the laptop.nix file. The touchpad support was previously enabled by default in most desktop managers. The changes include:
    
    - Commenting out the entire `services.xserver` block
    - Disabling libinput.enable
    - Disabling inputClassSections configuration for touchpad settings such as tapping, palm detection, palm size, and palm pressure

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -6,21 +6,21 @@
   ];
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver = {
-    libinput.enable = true;
-    inputClassSections = [
-      ''
-        Section "InputClass"
-          Identifier "libinput touchpad catchall"
-          MatchIsTouchpad "on"
-          MatchDevicePath "/dev/input/event*"
-          Driver "libinput"
-          Option "Tapping" "on"
-          Option "PalmDetection" "1"
-          Option "PalmSize" "30" # Adjust this value if needed
-          Option "PalmPressure" "100" # Adjust this value if needed
-        EndSection
-      ''
-    ];
-  };
+  # services.xserver = {
+  #   libinput.enable = true;
+  #   inputClassSections = [
+  #     ''
+  #       Section "InputClass"
+  #         Identifier "libinput touchpad catchall"
+  #         MatchIsTouchpad "on"
+  #         MatchDevicePath "/dev/input/event*"
+  #         Driver "libinput"
+  #         Option "Tapping" "on"
+  #         Option "PalmDetection" "1"
+  #         Option "PalmSize" "30" # Adjust this value if needed
+  #         Option "PalmPressure" "100" # Adjust this value if needed
+  #       EndSection
+  #     ''
+  #   ];
+  # };
 }
