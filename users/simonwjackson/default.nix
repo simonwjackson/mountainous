361f91aff20a6063bb705ca12b64ae0a028bb7a5
Simon W. Jackson <hello@simonwjackson.io>
2023-04-14 17:21:46 -0500

    :recycle: refactor(main-user): move sessionVariables to main-user.nix
    - Moved FZF_DEFAULT_OPTS
    - Moved MPV_SOCKET
    - Moved GITHUB_TOKEN, GITHUB_USER
    - Moved OPENAI_API_KEY
    - Moved GDK_SCALE, GDK_DPI_SCALE
    - Moved QT_AUTO_SCREEN_SET_FACTOR, QT_QPA_PLATFORMTHEME
    - Moved QT_SCALE_FACTOR, QT_FONT_DPI

--- users/simonwjackson/default.nix
+++ users/simonwjackson/default.nix
@@ -30,20 +30,6 @@ in
       ./unzen-taskwarrior-sync.nix
     ];
 
-    home.sessionVariables = {
-      FZF_DEFAULT_OPTS = "--reverse --layout=reverse --color=bg+:-1 --ansi --marker='❖' --pointer='❯' --prompt='  '";
-      MPV_SOCKET = "/run/user/\$(id -u)/mpv.socket";
-      GITHUB_TOKEN = builtins.getEnv ("GITHUB_TOKEN");
-      GITHUB_USER = builtins.getEnv ("GITHUB_USER");
-      OPENAI_API_KEY = builtins.getEnv ("OPENAI_API_KEY");
-      GDK_SCALE = if (builtins.getEnv ("NIX_CONFIG_HIDPI") == "1") then 2 else 1;
-      GDK_DPI_SCALE = if (builtins.getEnv ("NIX_CONFIG_HIDPI") == "1") then 0.5 else 1;
-      QT_AUTO_SCREEN_SET_FACTOR = 1;
-      QT_QPA_PLATFORMTHEME = "qt5ct";
-      QT_SCALE_FACTOR = if (builtins.getEnv ("NIX_CONFIG_HIDPI") == "1") then 2 else 1;
-      QT_FONT_DPI = 96;
-    };
-
     programs.mpvd.enable = true;
     programs.media-control.enable = true;
     programs.fuzzy-music.enable = true;
