b0a0ce858f193f5ed357a758e67942e4e030816f
Simon W. Jackson <hello@simonwjackson.io>
2023-04-22 23:14:24 -0500

    :sparkles: feat(awesome): add aria2 status widget and update configs
    - Add aria2 status widget to the awesome desktop config
    - Update bars, errors, layouts, rc, rules, signals, and themes configs
    - Add new files for the awesome desktop configuration

---
+++ profiles/gui/desktop/awesome/themes.lua
@@ -0,0 +1,6 @@
+local gears = require("gears")
+local beautiful = require("beautiful")
+--
+-- {{{ Variable definitions
+-- Themes define colours, icons, font and wallpapers.
+-- beautiful.init("/home/simonwjackson/.config/awesome/themes/default/theme.luas")
