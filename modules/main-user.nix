92c6bfb6245096f8163c26d80d7540cdd76dfc30
Simon W. Jackson <hello@simonwjackson.io>
2023-04-23 21:20:29 -0500

    :recycle: refactor: move environment variables to xserver.nix
    - Remove GDK_SCALE, GDK_DPI_SCALE, QT_AUTO_SCREEN_SET_FACTOR, QT_QPA_PLATFORMTHEME, QT_SCALE_FACTOR, and QT_FONT_DPI from main-user.nix
    - Add sessionVariables with GDK_SCALE, GDK_DPI_SCALE, QT_AUTO_SCREEN_SET_FACTOR, QT_QPA_PLATFORMTHEME, QT_SCALE_FACTOR, and QT_FONT_DPI to xserver.nix
    - Update Xft.dpi and *.dpi values in xresources properties

--- modules/main-user.nix
+++ modules/main-user.nix
@@ -21,7 +21,6 @@ in
   };
 
   home-manager.users.simonwjackson = { config, pkgs, ... }: {
-
     home = {
       stateVersion = "23.05";
       username = "simonwjackson";
@@ -32,12 +31,6 @@ in
         GITHUB_TOKEN = builtins.getEnv ("GITHUB_TOKEN");
         GITHUB_USER = builtins.getEnv ("GITHUB_USER");
         OPENAI_API_KEY = builtins.getEnv ("OPENAI_API_KEY");
-        GDK_SCALE = if (builtins.getEnv ("NIX_CONFIG_HIDPI") == "1") then 2 else 1;
-        GDK_DPI_SCALE = if (builtins.getEnv ("NIX_CONFIG_HIDPI") == "1") then 0.5 else 1;
-        QT_AUTO_SCREEN_SET_FACTOR = 1;
-        QT_QPA_PLATFORMTHEME = "qt5ct";
-        QT_SCALE_FACTOR = if (builtins.getEnv ("NIX_CONFIG_HIDPI") == "1") then 2 else 1;
-        QT_FONT_DPI = 96;
       };
     };
   };
