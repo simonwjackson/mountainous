af1d03dbf554c97694e1899759cf871dcd8ee99a
Simon W. Jackson <hello@simonwjackson.io>
2023-04-26 16:56:05 -0500

    :sparkles: feat(picom): add wintypes and opacity-rule options
    - Add wintypes configuration for normal windows
    - Add opacity-rule for _NET_WM_STATE_HIDDEN windows

--- profiles/gui/desktop/picom/picom.nix
+++ profiles/gui/desktop/picom/picom.nix
@@ -6,6 +6,14 @@
 
   active-opacity = .8;
   inactive-opacity = .8;
+  wintypes = {
+    normal = { opacity = .8; };
+  };
+
+  opacity-rule = [
+    "0:_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'"
+  ];
+
   blur-background = true;
   blur-method = "dual_kawase";
   blur-strength = 8;
