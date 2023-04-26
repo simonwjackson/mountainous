d60872aaf36a0b4635f3972c206db6319bd251d9
Simon W. Jackson <hello@simonwjackson.io>
2023-04-26 16:53:43 -0500

    :sparkles: feat(neovim/plugins): update lf.lua and lfrc configs
    - Update default_cmd comment in lf.lua
    - Remove extra newline in lf.lua
    - Update FloatBorder guifg comment in lf.lua
    - Add new keybindings and commands in lfrc
    - Set previewer and icons in lfrc

--- modules/neovim/plugins/lf.lua
+++ modules/neovim/plugins/lf.lua
@@ -1,6 +1,6 @@
 local wk = require("which-key")
 require("lf").setup({
-  default_cmd = "lf", -- default `lf` command
+  default_cmd = "lf",      -- default `lf` command
   default_action = "edit", -- default action when `Lf` opens a file
   default_actions = {
     -- default action keybindings
@@ -9,29 +9,27 @@ require("lf").setup({
     ["<C-v>"] = "vsplit",
     ["<C-o>"] = "tab drop",
   },
-
-  winblend = 10, -- psuedotransparency level
-  dir = "", -- directory where `lf` starts ('gwd' is git-working-directory, "" is CWD)
-  direction = "float", -- window type: float horizontal vertical
-  border = "single", -- border kind: single double shadow curved
-  height = 0.80, -- height of the *floating* window
-  width = 0.85, -- width of the *floating* window
-  escape_quit = true, -- map escape to the quit command (so it doesn't go into a meta normal mode)
+  winblend = 10,         -- psuedotransparency level
+  dir = "",              -- directory where `lf` starts ('gwd' is git-working-directory, "" is CWD)
+  direction = "float",   -- window type: float horizontal vertical
+  border = "single",     -- border kind: single double shadow curved
+  height = 0.80,         -- height of the *floating* window
+  width = 0.85,          -- width of the *floating* window
+  escape_quit = true,    -- map escape to the quit command (so it doesn't go into a meta normal mode)
   focus_on_open = false, -- focus the current file when opening Lf (experimental)
-  mappings = true, -- whether terminal buffer mapping is enabled
-  tmux = false, -- tmux statusline can be disabled on opening of Lf
+  mappings = true,       -- whether terminal buffer mapping is enabled
+  tmux = false,          -- tmux statusline can be disabled on opening of Lf
   -- highlights passed to toggleterm
   highlights = {
     -- Normal = { guibg = <VALUE> },
     NormalFloat = { link = 'Normal' },
     FloatBorder = {
-      guifg = require("dracula").colors().comment,
+      -- guifg = require("dracula").colors().comment,
     }
   },
   -- Layout configurations
   -- resize window with this key
   layout_mapping = "<A-u>",
-
   views = { -- window dimensions to rotate through
     { width = 0.600, height = 0.600 },
     -- {
