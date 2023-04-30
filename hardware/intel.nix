bd7a63b01b40ce73bc3b568ef038b262806fd2ae
Simon W. Jackson <hello@simonwjackson.io>
2023-04-30 03:14:23 +0000

    :gear: refactor: move hostPlatform setting to hardware/intel.nix
    - :wrench: remove redundant kernelModules and extraModulePackages
    - :truck: move nixpkgs.hostPlatform setting from systems/yari to hardware/intel

--- hardware/intel.nix
+++ hardware/intel.nix
@@ -1,5 +1,6 @@
 { config, lib, ... }:
 
 {
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- systems/yari/default.nix
+++ systems/yari/default.nix
@@ -17,15 +17,12 @@
   ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
   boot.kernelModules = [ "kvm-intel" "uinput" ];
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
   # Includes the Wi-Fi and Bluetooth firmware
   hardware.enableRedistributableFirmware = true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
