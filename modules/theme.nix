2c2410c65d882c1dd254fe28ac9522c2be2b7f76
Simon W. Jackson <hello@simonwjackson.io>
2023-05-11 09:25:43 -0500

    :hammer: chore: update file permissions and add theme module
    - Changed file permissions for various scripts
    - Added new theme.nix module for home-manager configuration
    - Updated packages and profiles with new file permissions

---
+++ modules/theme.nix
@@ -0,0 +1,27 @@
+{ ... }:
+
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
+{
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  home-manager.users.simonwjackson = { config, pkgs, ... }: {
+    systemd.user.services.autorandr = {
+      Unit = {
+        Description = "Set pywal wallpaper at startup";
+        After = "graphical-session.target";
+      };
+      Service = {
+        Type = "oneshot";
+        Environment = "DISPLAY=:0";
+        ExecStart = "${pkgs.python3Packages.pywal}/bin/wal -R";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
+  };
+}
