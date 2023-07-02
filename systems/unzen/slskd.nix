58a59357e0ae71a03d56d24dfd2e6aa6cda8476f
Simon W. Jackson <hello@simonwjackson.io>
2023-07-01 21:56:45 -0500

    :twisted_rightwards_arrows: refactor(system): comment out slskd-beets-import service
    - The systemd service `slskd-beets-import` has been commented out
    - This service was initially responsible for running `beet import` when a slskd download completes
    - The changes have been made in the `systems/unzen/slskd.nix` file

--- systems/unzen/slskd.nix
+++ systems/unzen/slskd.nix
@@ -30,30 +30,30 @@ in
       };
     };
 
-    systemd.services.slskd-beets-import = {
-      description = "When a slskd download completes, run `beet import`";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-      serviceConfig = {
-        Type = "simple";
-        User = "simonwjackson";
-        Group = "users";
-        Restart = "on-failure";
-        ExecStart = "${pkgs.writeScript "slskd-beets-import.sh" ''
-          #!/bin/sh
-
-          EVENTS="create,moved_to"
-
-          ${pkgs.inotify-tools}/bin/inotifywait -m -r -e $EVENTS "${toString audioDownloads}" | while read -r watched_path event file; do
-            echo "[$(date)] Event: $event, File: $watched_path$file"
-
-            ${pkgs.beets}/bin/beet import -m -q ${toString audioDownloads} \
-            && ${pkgs.beets}/bin/beet duplicates --delete \
-            && ${pkgs.beets}/bin/beet update \
-            && ${pkgs.beets}/bin/beet convert -y
-          done
-        ''}";
-      };
-    };
+    # systemd.services.slskd-beets-import = {
+    #   description = "When a slskd download completes, run `beet import`";
+    #   wantedBy = [ "multi-user.target" ];
+    #   after = [ "network.target" ];
+    #   serviceConfig = {
+    #     Type = "simple";
+    #     User = "simonwjackson";
+    #     Group = "users";
+    #     Restart = "on-failure";
+    #     ExecStart = "${pkgs.writeScript "slskd-beets-import.sh" ''
+    #       #!/bin/sh
+    #
+    #       EVENTS="create,moved_to"
+    #
+    #       ${pkgs.inotify-tools}/bin/inotifywait -m -r -e $EVENTS "${toString audioDownloads}" | while read -r watched_path event file; do
+    #         echo "[$(date)] Event: $event, File: $watched_path$file"
+    #
+    #         ${pkgs.beets}/bin/beet import -m -q ${toString audioDownloads} \
+    #         && ${pkgs.beets}/bin/beet duplicates --delete \
+    #         && ${pkgs.beets}/bin/beet update \
+    #         && ${pkgs.beets}/bin/beet convert -y
+    #       done
+    #     ''}";
+    #   };
+    # };
   };
 }
