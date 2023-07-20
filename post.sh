423c0857ac8064e4a6cd6438a9495ff5e698cc8c
Simon W. Jackson <hello@simonwjackson.io>
2023-07-19 22:17:00 -0500

    :lock: fix(ssh): comment out ssh key generation lines
    - Commented out lines that were previously writing the SSH keys to disk and adding them to the authorized_keys file. This change improves the security of the script by not exposing the private keys.

--- post.sh
+++ post.sh
@@ -5,14 +5,14 @@ set -e
 
 [[ $(op account get) ]] || eval "$(op signin)"
 
-echo "${SSH_PERSONAL_RSA_PRIVATE}" > "${HOME}/.ssh/id_rsa"
-echo "${SSH_CLERK_ED25519_PRIVATE}" > "${HOME}/.ssh/id_ed25519"
+# echo "${SSH_PERSONAL_RSA_PRIVATE}" > "${HOME}/.ssh/id_rsa"
+# echo "${SSH_CLERK_ED25519_PRIVATE}" > "${HOME}/.ssh/id_ed25519"
 
 chmod 600 "${HOME}/.ssh/id_rsa"
 chmod 600 "${HOME}/.ssh/id_ed25519"
 
-ssh-keygen -y -f ~/.ssh/id_rsa > "${HOME}/.ssh/authorized_keys"
-ssh-keygen -y -f ~/.ssh/id_ed25519 >> "${HOME}/.ssh/authorized_keys"
+# ssh-keygen -y -f ~/.ssh/id_rsa > "${HOME}/.ssh/authorized_keys"
+# ssh-keygen -y -f ~/.ssh/id_ed25519 >> "${HOME}/.ssh/authorized_keys"
 
 # TODO: Find a better way to install pip packages
 # nix-shell -p python3.pkgs.pip --run 'pip install --user -r requirements.txt'
