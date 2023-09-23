a08b19bea2771fd2be8629b25fba0d60593dc640
Simon W. Jackson <hello@simonwjackson.io>
2023-09-23 17:02:59 -0500

    feat(unzen): add print server

--- systems/unzen/default.nix
+++ systems/unzen/default.nix
@@ -292,4 +292,19 @@
   # WARN: This speeds up `nixos-rebuild`, but im not sure if there are any side effects
   systemd.services.NetworkManager-wait-online.enable = false;
 
+
+  services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ brlaser ];
+
+  # Enable automatic discovery of the printer from other Linux systems with avahi running.
+  services.avahi.enable = true;
+  services.avahi.publish.enable = true;
+  services.avahi.publish.userServices = true;
+  services.printing.browsing = true;
+  services.printing.listenAddresses = [ "*:631" ]; # Not 100% sure this is needed and you might want to restrict to the local network
+  services.printing.allowFrom = [ "all" ]; # this gives access to anyone on the interface you might want to limit it see the official documentation
+  services.printing.defaultShared = true; # If you want
+
+  networking.firewall.allowedUDPPorts = [ 631 ];
+  networking.firewall.allowedTCPPorts = [ 631 ];
 }
