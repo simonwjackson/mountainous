aac79c822fac6b2ef9873b51158f3cfae1047d64
Simon W. Jackson <hello@simonwjackson.io>
2023-09-23 15:50:03 -0500

    feat: add taskwiki generators

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,7 +1,58 @@
-{ ... }:
+{ pkgs, ... }:
 
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  generate-tasks-next-markdown = pkgs.writeShellScriptBin "generate-tasks-next-markdown" ''
+    set -euo pipefail
+
+    MARKDOWN_FILE="$HOME/documents/notes/NEXT.md"
+    
+    projects=$(task +PENDING export |  jq -r '.[] | select(.project != null) | .project' | sort | uniq)
+    
+    for project in $projects
+    do
+        output+="###### $project | project.is:$project project:$project ((+UNBLOCKED +PENDING) or (+WAITING +UNBLOCKED)) "
+        output+='$U'
+        output+="\n\n"
+    done
+    
+    touch "$MARKDOWN_FILE"
+    echo -e "$output" > "$MARKDOWN_FILE" && \
+    nvim --headless -n -c "edit $MARKDOWN_FILE" -c "write" -c "quitall"
+  '';
+
+  generate-tasks-review-markdown = pkgs.writeShellScriptBin "generate-tasks-review-markdown" ''
+    set -euo pipefail
+
+    REVIEW_FILE="$HOME/documents/notes/REVIEW.md"
+    
+    output=""
+    projects=$(${pkgs.taskwarrior}/bin/task +PENDING export |  jq -r '.[] | select(.project != null) | .project' | sort | uniq)
+    
+    output+="###### No Project | +PENDING project:"" \n\n"
+
+    output+="###### Due | +PENDING +ai \n\n"
+    
+    output+="###### AI Review | +PENDING +ai \n\n"
+    
+    for project in $projects
+    do
+        output+="###### $project | project.is:$project project:$project\n\n"
+    done
+    
+    completed_projects=$(${pkgs.taskwarrior}/bin/task -PENDING export |  jq -r '.[] | select(.project != null) | .project' | sort | uniq)
+    
+    output+="###### Completed\n\n"
+    
+    for project in $completed_projects
+    do
+        output+="###### $project || project.is:$project project:$project\n\n"
+    done
+    
+    touch "$REVIEW_FILE"
+    echo -e "$output" > "$REVIEW_FILE" && \
+    ${pkgs.neovim}/bin/nvim --headless -n -c "edit $REVIEW_FILE" -c "write" -c "quitall"
+  '';
 in
 {
   imports = [
@@ -13,6 +64,9 @@ in
       unzip
       neovim-remote
       xclip
+    ] ++ [
+      generate-tasks-review-markdown
+      generate-tasks-next-markdown
     ];
     
     nixpkgs.overlays = [
