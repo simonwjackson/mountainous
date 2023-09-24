9a24b3822f30e8a681016b6c268fbfb51c333f53
Simon W. Jackson <hello@simonwjackson.io>
2023-09-23 21:02:04 -0500

    :recycle: refactor(flake): update flake inputs and restructure host configurations
    - :heavy_plus_sign: Added `home-manager` and `nixpkgs-stable` as new flake inputs
    - :arrows_counterclockwise: Switched `nixpkgs` input from indirect to GitHub type
    - :wrench: Adjusted `nixosConfigurations` in flake.nix to include `specialArgs` and changed system paths
    - :truck: Moved system configurations from `systems/` to `hosts/` directory
    - :hammer: Updated `flake.lock` with new input dependencies and changes

--- flake.nix
+++ flake.nix
@@ -2,31 +2,50 @@
   description = "Nix config flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, ... }: {
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    # Supported systems for your flake packages, shell, etc.
+    systems = [
+      "aarch64-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    # This is a function that generates an attribute by calling a function you
+    # pass to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
     nixosConfigurations = {
       # Home Server
       unzen = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        modules = [ ./systems/unzen ];
+        specialArgs = {inherit inputs outputs;};
+        modules = [ ./hosts/unzen ];
       };
 
       # Gaming
       zao = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        modules = [ ./systems/zao ];
+        specialArgs = {inherit inputs outputs;};
+        modules = [ ./hosts/zao ];
       };
 
       # Laptop
       fiji = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        modules = [ ./systems/fiji ];
+        specialArgs = {inherit inputs outputs;};
+        modules = [ ./hosts/fiji ];
       };
     };
   };
