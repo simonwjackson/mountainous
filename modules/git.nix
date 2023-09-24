7375da7789e32878a37b69b5dbe8335e7f204767
Simon W. Jackson <hello@simonwjackson.io>
2023-09-23 20:32:24 -0500

    chore: update lazygit config

--- modules/git.nix
+++ modules/git.nix
@@ -1,10 +1,7 @@
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-in
-{
-  imports = [
-    (import "${home-manager}/nixos")
-  ];
+in {
+  imports = [ (import "${home-manager}/nixos") ];
 
   home-manager.users.simonwjackson = { config, pkgs, ... }: {
     programs.lazygit = {
@@ -35,165 +32,192 @@ in
         ];
         disableStartupPopups = false;
         git = {
-          commit = {
-            signOff = false;
-            verbose = "default";
-          };
+          allBranchesLogCmd =
+            "git log --graph --all --color=always --abbrev-commit --decorate --date=relative  --pretty=medium";
+          autoFetch = true;
+          autoRefresh = true;
+          branchLogCmd =
+            "git log --graph --color=always --abbrev-commit --decorate --date=relative --pretty=medium {{branchName}} --";
+          commit = { signOff = false; };
+          disableForcePushing = false;
+          fetchAll = true;
           log = {
-            allBranchesLogCmd =
-              "git log --graph --all --color=always --abbrev-commit --decorate --date=relative  --pretty=medium";
-            autoFetch = true;
-            autoRefresh = true;
-            branchLogCmd =
-              "git log --graph --color=always --abbrev-commit --decorate --date=relative --pretty=medium {{branchName}} --";
-            diffContextSize = 3;
-            disableForcePushing = false;
             order = "topo-order";
-            overrideGpg = false;
-            parseEmoji = false;
             showGraph = "when-maximised";
             showWholeGraph = false;
-            skipHookPrefix = "WIP";
           };
+          mainBranches = [ "master" "main" ];
           merging = {
             args = "";
             manualCommit = false;
           };
+          overrideGpg = false;
           paging = {
             colorArg = "always";
             pager = "delta --dark --paging=never";
             useConfig = false;
           };
+          parseEmoji = false;
+          skipHookPrefix = "WIP";
         };
         gui = {
+          animateExplosion = true;
+          border = "rounded";
           commandLogSize = 8;
           commitLength = { show = true; };
           expandFocusedSidePanel = false;
-          language = "en";
+          language = "auto";
           mainPanelSplitMode = "flexible";
           mouseEvents = true;
+          nerdFontsVersion = "";
           scrollHeight = 2;
+          scrollOffBehavior = "margin";
+          scrollOffMargin = 2;
           scrollPastBottom = true;
+          shortTimeFormat = "3:04PM";
           showBottomLine = true;
+          showBranchCommitHash = false;
           showCommandLog = true;
           showFileTree = true;
           showIcons = false;
           showListFooter = true;
+          showPanelJumps = true;
           showRandomTip = true;
           sidePanelWidth = 0.3333;
+          skipDiscardChangeWarning = false;
+          skipRewordInEditorWarning = false;
           skipStashWarning = false;
-          skipUnstageLineWarning = false;
           splitDiff = "auto";
           theme = {
             activeBorderColor = [ "green" "bold" ];
-            cherryPickedCommitBgColor = [ "cyan" ];
-            cherryPickedCommitFgColor = [ "blue" ];
-            defaultFgColor = [ "default" ];
             inactiveBorderColor = [ "white" ];
+            searchingActiveBorderColor = [ "cyan" "bold" ];
             optionsTextColor = [ "blue" ];
             selectedLineBgColor = [ "blue" ];
             selectedRangeBgColor = [ "blue" ];
+            cherryPickedCommitBgColor = [ "cyan" ];
+            cherryPickedCommitFgColor = [ "blue" ];
             unstagedChangesColor = [ "red" ];
+            defaultFgColor = [ "default" ];
           };
-          timeFormat = "02 Jan 06 15:04 MST";
+          timeFormat = "02 Jan 06";
+          windowSize = "normal";
         };
         keybinding = {
+          branches = {
+            checkoutBranchByName = "c";
+            createPullRequest = "o";
+            createTag = "T";
+            fastForward = "f";
+            fetchRemote = "f";
+            forceCheckoutBranch = "F";
+            mergeIntoCurrentBranch = "M";
+            pushTag = "P";
+            rebaseBranch = "r";
+            renameBranch = "R";
+            setUpstream = "u";
+            viewGitFlowOptions = "i";
+            viewPullRequestOptions = "O";
+          };
+          commitFiles = { checkoutCommitFile = "c"; };
+          commits = {
+            amendToCommit = "A";
+            checkoutCommit = "<space>";
+            cherryPickCopy = "c";
+            cherryPickCopyRange = "C";
+            copyCommitMessageToClipboard = "<c-y>";
+            createFixupCommit = "F";
+            markCommitAsFixup = "f";
+            moveDownCommit = "<c-j>";
+            moveUpCommit = "<c-k>";
+            openLogMenu = "<c-l>";
+            pasteCommits = "v";
+            pickCommit = "p";
+            renameCommit = "r";
+            renameCommitWithEditor = "R";
+            resetCherryPick = "<c-R>";
+            revertCommit = "t";
+            squashAboveCommits = "S";
+            squashDown = "s";
+            tagCommit = "T";
+            viewBisectOptions = "b";
+            viewResetOptions = "g";
+          };
+          files = {
+            amendLastCommit = "A";
+            commitChanges = "c";
+            commitChangesWithEditor = "C";
+            commitChangesWithoutHook = "w";
+            confirmDiscard = "x";
+            fetch = "f";
+            ignoreFile = "i";
+            openMergeTool = "M";
+            openStatusFilter = "<c-b>";
+            refreshFiles = "r";
+            stashAllChanges = "s";
+            toggleStagedAll = "a";
+            toggleTreeView = "`";
+            viewResetOptions = "D";
+            viewStashOptions = "S";
+          };
+          main = {
+            "toggleDragSelect-alt" = "V";
+            pickBothHunks = "b";
+            toggleDragSelect = "v";
+            toggleSelectHunk = "a";
+          };
+          stash = {
+            popStash = "g";
+            renameStash = "r";
+          };
+          status = {
+            checkForUpdate = "u";
+            recentRepos = "<enter>";
+          };
+          submodules = {
+            bulkMenu = "b";
+            init = "i";
+            update = "u";
+          };
           universal = {
-            appendNewline = "<a-enter>";
-            branches = {
-              checkoutBranchByName = "c";
-              createPullRequest = "o";
-              fastForward = "f";
-              fetchRemote = "f";
-              forceCheckoutBranch = "F";
-              mergeIntoCurrentBranch = "M";
-              pushTag = "P";
-              rebaseBranch = "r";
-              renameBranch = "R";
-              setUpstream = "u";
-              viewGitFlowOptions = "i";
-              viewPullRequestOptions = "O";
-            };
-            commitFiles = { checkoutCommitFile = "c"; };
-            commits = {
-              amendToCommit = "A";
-              checkoutCommit = "<space>";
-              cherryPickCopy = "c";
-              cherryPickCopyRange = "C";
-              copyCommitMessageToClipboard = "<c-y>";
-              createFixupCommit = "F";
-              markCommitAsFixup = "f";
-              moveDownCommit = "<c-j>";
-              moveUpCommit = "<c-k>";
-              openLogMenu = "<c-l>";
-              pasteCommits = "v";
-              pickCommit = "p";
-              renameCommit = "r";
-              renameCommitWithEditor = "R";
-              resetCherryPick = "<c-R>";
-              revertCommit = "t";
-              squashAboveCommits = "S";
-              squashDown = "s";
-              tagCommit = "T";
-              viewBisectOptions = "b";
-              viewResetOptions = "g";
-            };
+            "diffingMenu-alt" = "<c-e>";
+            "nextBlock-alt" = "l";
+            "nextItem-alt" = "j";
+            "optionMenu-alt1" = "?";
+            "prevBlock-alt" = "h";
+            "prevItem-alt" = "k";
+            "quit-alt1" = "<c-c>";
+            "scrollDownMain-alt1" = "J";
+            "scrollDownMain-alt2" = "<c-d>";
+            "scrollUpMain-alt1" = "K";
+            "scrollUpMain-alt2" = "<c-u>";
             confirm = "<enter>";
-            "confirm-alt1" = "y";
             copyToClipboard = "<c-o>";
             createPatchOptionsMenu = "<c-p>";
             createRebaseOptionsMenu = "m";
             decreaseContextInDiffView = "{";
             diffingMenu = "W";
-            "diffingMenu-alt" = "<c-e>";
             edit = "e";
             executeCustomCommand = ":";
             extrasMenu = "@";
-            files = {
-              amendLastCommit = "A";
-              commitChanges = "C";
-              commitChangesWithEditor = "c";
-              commitChangesWithoutHook = "w";
-              fetch = "f";
-              ignoreFile = "i";
-              openMergeTool = "M";
-              openStatusFilter = "<c-b>";
-              refreshFiles = "r";
-              stashAllChanges = "s";
-              toggleStagedAll = "a";
-              toggleTreeView = "`";
-              viewResetOptions = "D";
-              viewStashOptions = "S";
-            };
             filteringMenu = "<c-s>";
             goInto = "<enter>";
             gotoBottom = ">";
             gotoTop = "<";
             increaseContextInDiffView = "}";
             jumpToBlock = [ "1" "2" "3" "4" "5" ];
-            main = {
-              pickBothHunks = "b";
-              toggleDragSelect = "v";
-              "toggleDragSelect-alt" = "V";
-              toggleSelectHunk = "a";
-            };
             new = "n";
             nextBlock = "<right>";
-            "nextBlock-alt" = "l";
             nextItem = "<down>";
-            "nextItem-alt" = "j";
             nextMatch = "n";
             nextPage = ".";
             nextScreenMode = "+";
             nextTab = "]";
             openFile = "o";
             openRecentRepos = "<c-r>";
-            optionMenu = "x";
-            "optionMenu-alt1" = "?";
+            optionMenu = null;
             prevBlock = "<left>";
-            "prevBlock-alt" = "h";
             prevItem = "<up>";
-            "prevItem-alt" = "k";
             prevMatch = "N";
             prevPage = ",";
             prevScreenMode = "_";
@@ -201,35 +225,17 @@ in
             pullFiles = "p";
             pushFiles = "P";
             quit = "q";
-            "quit-alt1" = "<c-c>";
             quitWithoutChangingDirectory = "Q";
             redo = "<c-z>";
             refresh = "R";
             remove = "d";
             return = "<esc>";
             scrollDownMain = "<pgdown>";
-            "scrollDownMain-alt1" = "J";
-            "scrollDownMain-alt2" = "<c-d>";
             scrollLeft = "H";
             scrollRight = "L";
             scrollUpMain = "<pgup>";
-            "scrollUpMain-alt1" = "K";
-            "scrollUpMain-alt2" = "<c-u>";
             select = "<space>";
-            stash = {
-              popStash = "g";
-              renameStash = "r";
-            };
-            status = {
-              checkForUpdate = "u";
-              recentRepos = "<enter>";
-            };
             submitEditorText = "<enter>";
-            submodules = {
-              bulkMenu = "b";
-              init = "i";
-              update = "u";
-            };
             togglePanel = "<tab>";
             toggleWhitespaceInDiffView = "<c-w>";
             undo = "z";
@@ -237,9 +243,13 @@ in
         };
         notARepository = "prompt";
         os = {
-          editCommand = "";
-          editCommandTemplate = "";
-          openCommand = "";
+          copyToClipboardCmd = "";
+          edit = "";
+          editAtLine = "";
+          editAtLineAndWait = "";
+          editPreset = "";
+          open = "";
+          openLink = "";
         };
         promptToReturnFromSubprocess = false;
         quitOnTopLevelReturn = false;
@@ -310,7 +320,8 @@ in
         amend-all-now = "commit --amend --no-edit -a";
 
         fzf = "!f() { git-fzf $@ }; f";
-        edit-modified = "! git status --porcelain  | awk '{print $2}' | xargs -I '%' echo \"$(git rev-parse --show-toplevel)/%\" | xargs nvim -p";
+        edit-modified = ''
+          ! git status --porcelain  | awk '{print $2}' | xargs -I '%' echo "$(git rev-parse --show-toplevel)/%" | xargs nvim -p'';
         em = "edit-modified";
 
         # Graph log: gives you history of current branch
@@ -320,53 +331,54 @@ in
         # Graph log all as shortlist: tag/branch/labelled commits.
         las = "log --graph --decorate --oneline --all --simplify-by-decoration";
         # Graph who and when.
-        lw = "log --color --graph --pretty=format:'%C(214)%h%C(reset)%C(196)%d%C(reset) %s %C(35)(%cr)%C(27) <%an>%C(reset)'";
+        lw =
+          "log --color --graph --pretty=format:'%C(214)%h%C(reset)%C(196)%d%C(reset) %s %C(35)(%cr)%C(27) <%an>%C(reset)'";
         # Escape < and > for github markdown, (useful for generating changelogs).
-        changelog = "! git log --pretty=format:'* %h - %s %n%w(76,4,4)%b%n' --abbrev-commit \"$@\" | perl -0 -p -e 's/(^|[^\\\\])([<>])/\\1\\\\\\2/g ; s/(\\s*\\n)+\\*/\\n*/g' #";
+        changelog = ''
+          ! git log --pretty=format:'* %h - %s %n%w(76,4,4)%b%n' --abbrev-commit "$@" | perl -0 -p -e 's/(^|[^\\])([<>])/\1\\\2/g ; s/(\s*\n)+\*/\n*/g' #'';
         sync = "! git fetch --tags && git rebase --autostash && git push";
 
-        gpt = "!f() { git diff --staged | tr -s '[:space:]' '\n' | head -n 3500 | tr -s '[:space:]' ' ' | sgpt --model gpt-4 --temperature .5 \"Generate a conventional commit from these chages. template: ':emoji: <type>[optional scope]: <description>\n\n[optional body]'. Body is a list. Prepend first line with a gitmoji directly related to the type. ideally, first line is lowercase 50 chars or less\" | tee /dev/tty | (git commit -F - --edit || true); }; f";
-        gpt-pr = "!f() { git log main..HEAD --pretty=format:\"%h %s%n%n%b\" | sgpt --model gpt-4 \"Here are my commit messages. Use them to write a summary and a list summarized chages. Each item will be prepended with an emoji and commit  hash. Write informally and imperatively. Format: <summary>\n\n<list>\"; }; f";
+        gpt = ''
+          !f() { git diff --staged | tr -s '[:space:]' '
+          ' | head -n 3500 | tr -s '[:space:]' ' ' | sgpt --model gpt-4 --temperature .5 "Generate a conventional commit from these chages. template: ':emoji: <type>[optional scope]: <description>
+
+          [optional body]'. Body is a list. Prepend first line with a gitmoji directly related to the type. ideally, first line is lowercase 50 chars or less" | tee /dev/tty | (git commit -F - --edit || true); }; f'';
+        gpt-pr = ''
+          !f() { git log main..HEAD --pretty=format:"%h %s%n%n%b" | sgpt --model gpt-4 "Here are my commit messages. Use them to write a summary and a list summarized chages. Each item will be prepended with an emoji and commit  hash. Write informally and imperatively. Format: <summary>
+
+          <list>"; }; f'';
 
         # Squash all unpushed commits with a new message
-        squash = "! git reset --soft HEAD~$(git log origin/main..main | grep commit | wc -l | awk '{$1=$1};1') && git commit";
+        squash =
+          "! git reset --soft HEAD~$(git log origin/main..main | grep commit | wc -l | awk '{$1=$1};1') && git commit";
         s = "squash";
 
-        trunkit = "!f() { git stash --include-untracked && git fetch --all && git pull && git stash pop && git add --all && { [ -z \"$1\" ] && git gpt || git commit --message \"$1\"; } && git push; };f";
+        trunkit = ''
+          !f() { git stash --include-untracked && git fetch --all && git pull && git stash pop && git add --all && { [ -z "$1" ] && git gpt || git commit --message "$1"; } && git push; };f'';
 
         # Worktree
         wtl = "worktree list";
-        wta = "!f() { git show-ref --verify --quiet refs/heads/$1; local_branch_exists=$?; git ls-remote --exit-code --heads origin $1 > /dev/null 2>&1; remote_branch_exists=$?; if [ $local_branch_exists -eq 0 ]; then git worktree add $1 $1; elif [ $remote_branch_exists -eq 0 ]; then git worktree add -b $1 --track origin/$1 $1; else git worktree add -b $1 $1; fi }; f";
-        wtr = "!f() { printf \"Are you sure you want to remove branch (local & remote)? (y/n) \" && read -r REPLY && [ \"$REPLY\" = \"y\" -o \"$REPLY\" = \"Y\" ] && git worktree remove \"$1\" && git worktree prune && git branch -D \"$1\" && git push origin --delete \"$1\"; }; f";
-        wtc = "!f(){ mkdir $(basename \"\${1}\" .git); cd $(basename \"\${1}\" .git); git clone --bare \"\${1}\" .bare; echo 'gitdir: ./.bare' > .git; git worktree add main main; };f";
+        wta =
+          "!f() { git show-ref --verify --quiet refs/heads/$1; local_branch_exists=$?; git ls-remote --exit-code --heads origin $1 > /dev/null 2>&1; remote_branch_exists=$?; if [ $local_branch_exists -eq 0 ]; then git worktree add $1 $1; elif [ $remote_branch_exists -eq 0 ]; then git worktree add -b $1 --track origin/$1 $1; else git worktree add -b $1 $1; fi }; f";
+        wtr = ''
+          !f() { printf "Are you sure you want to remove branch (local & remote)? (y/n) " && read -r REPLY && [ "$REPLY" = "y" -o "$REPLY" = "Y" ] && git worktree remove "$1" && git worktree prune && git branch -D "$1" && git push origin --delete "$1"; }; f'';
+        wtc = ''
+          !f(){ mkdir $(basename "''${1}" .git); cd $(basename "''${1}" .git); git clone --bare "''${1}" .bare; echo 'gitdir: ./.bare' > .git; git worktree add main main; };f'';
       };
 
       extraConfig = {
-        difftool = {
-          prompt = false;
-        };
+        difftool = { prompt = false; };
 
-        diff = {
-          tool = "vimdiff";
-        };
+        diff = { tool = "vimdiff"; };
 
-        push = {
-          default = "current";
-        };
+        push = { default = "current"; };
 
-        pull = {
-          ff = "only";
-        };
+        pull = { ff = "only"; };
 
-        init = {
-          defaultBranch = "main";
-        };
+        init = { defaultBranch = "main"; };
 
         safe = {
-          directory = [
-            "/etc/nixos"
-            "/home/simonwjackson/nix-config"
-          ];
+          directory = [ "/etc/nixos" "/home/simonwjackson/nix-config" ];
         };
       };
 
