35927c4d3e5377b92a19d8a6fa970335e8586ad6
Simon W. Jackson <hello@simonwjackson.io>
2023-10-20 17:19:53 -0500

    :twisted_rightwards_arrows: refactor(flake.nix): switch to user-specific home-manager config
    - Uncommented `extraSpecialArgs` to inherit inputs, outputs, rootPath, and self
    - Replaced the existing `config` block with an import statement for user-specific home-manager config
    - Commented out the previous `config` block which included home state version, username, home directory, and packages
    - This change allows for more user-specific customization and easier management of home-manager configurations

--- flake.nix
+++ flake.nix
@@ -180,22 +180,22 @@
             home-manager = {
               # backupFileExtension = "hm-bak";
               # useGlobalPkgs = true;
-              # extraSpecialArgs = { inherit inputs outputs rootPath self; };
+              extraSpecialArgs = { inherit inputs outputs rootPath self; };
 
-              config =
-                { config, lib, pkgs, ... }:
-                {
-                  # Read the changelog before changing this value
-                  home.stateVersion = "23.05";
-                  home = {
-                    username = "nix-on-droid";
-                    homeDirectory = "/data/data/com.termux.nix/files/home";
-                    packages = with pkgs; [
-                      jq
-                    ];
-                  };
-                  # insert home-manager config
-                };
+              config = import ./home-manager/users/simonwjackson/hosts/usu;
+              # { config, lib, pkgs, ... }:
+              # {
+              #   # Read the changelog before changing this value
+              #   home.stateVersion = "23.05";
+              #   home = {
+              #     username = "nix-on-droid";
+              #     homeDirectory = "/data/data/com.termux.nix/files/home";
+              #     packages = with pkgs; [
+              #       jq
+              #     ];
+              #   };
+              #   # insert home-manager config
+              # };
             };
           }
           # home-manager.darwinModules.home-manager
