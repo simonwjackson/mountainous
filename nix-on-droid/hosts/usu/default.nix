8c1b6a14f1e631edc86f72335d43810fbc00320d
Simon W. Jackson <hello@simonwjackson.io>
2023-10-22 11:57:10 +0100

    :recycle: refactor(nix-on-droid): simplify package installation
    - Remove commented out packages
    - Add findutils, utillinux, tzdata, hostname, gnugrep, gnused to the environment.packages list

--- nix-on-droid/hosts/usu/default.nix
+++ nix-on-droid/hosts/usu/default.nix
@@ -19,26 +19,14 @@
 
   # Simply install just the packages
   environment.packages = with pkgs; [
-    # User-facing stuff that you really really want to have
-    vim # or some other editor, e.g. nano or neovim
     git
     openssh
-    # Some common stuff that people expect to have
-    #diffutils
-    #findutils
-    #utillinux
-    #tzdata
-    #hostname
-    #man
-    #gnugrep
-    #gnupg
-    #gnused
-    #gnutar
-    #bzip2
-    #gzip
-    #xz
-    #zip
-    #unzip
+    findutils
+    utillinux
+    tzdata
+    hostname
+    gnugrep
+    gnused
   ];
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
