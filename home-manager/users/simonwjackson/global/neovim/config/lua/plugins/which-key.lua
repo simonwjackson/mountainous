74cc2b907151b60e5b71f6dbde9133e3edb3c80f
Simon W. Jackson <hello@simonwjackson.io>
2023-10-27 18:31:29 +0100

    :sparkles: feat(neovim config): add which-key plugin configuration
    - Added new configuration for the which-key plugin in the neovim config file
    - Included several key mappings for various functions such as goto, next, prev, tabs, buffer, code, file/find, git, hunks, quit/session, search, noice, ui, windows, diagnostics/quickfix
    - The new configuration is expected to enhance user experience by providing a popup menu for available keybindings

--- home-manager/users/simonwjackson/global/neovim/config/lua/plugins/which-key.lua
+++ home-manager/users/simonwjackson/global/neovim/config/lua/plugins/which-key.lua
@@ -1,3 +1,33 @@
+-- {
+--   "folke/which-key.nvim",
+--   event = "VeryLazy",
+--   opts = {
+--     plugins = { spelling = true },
+--     key_labels = { ["<leader>"] = "SPC" },
+--   },
+--   config = function(_, opts)
+--     local wk = require("which-key")
+--     wk.setup(opts)
+--     wk.register({
+--       mode = { "n", "v" },
+--       ["g"] = { name = "+goto" },
+--       ["]"] = { name = "+next" },
+--       ["["] = { name = "+prev" },
+--       ["<leader><tab>"] = { name = "+tabs" },
+--       ["<leader>b"] = { name = "+buffer" },
+--       ["<leader>c"] = { name = "+code" },
+--       ["<leader>f"] = { name = "+file/find" },
+--       ["<leader>g"] = { name = "+git" },
+--       ["<leader>gh"] = { name = "+hunks" },
+--       ["<leader>q"] = { name = "+quit/session" },
+--       ["<leader>s"] = { name = "+search" },
+--       ["<leader>sn"] = { name = "+noice" },
+--       ["<leader>u"] = { name = "+ui" },
+--       ["<leader>w"] = { name = "+windows" },
+--       ["<leader>x"] = { name = "+diagnostics/quickfix" },
+--     })
+--   end,
+-- },
 local cmd = vim.api.nvim_command
 -- This is a lua version of vim which key that displays available keybindings in popup menu.
 
