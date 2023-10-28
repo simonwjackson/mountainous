758ce070dc8d674b9068a118c5b43b6fef81b0a2
Simon W. Jackson <hello@simonwjackson.io>
2023-10-28 13:13:15 +0100

    :twisted_rightwards_arrows: refactor(neovim config): update gitsigns and which-key plugins
    - Move gitsigns configuration into its own function
    - Add dependency on which-key for gitsigns
    - Add on_attach function to gitsigns for hunk navigation and actions
    - Register new key mappings for gitsigns in the which-key plugin
    - Remove redundant hunk navigation and actions from the which-key plugin

--- home-manager/users/simonwjackson/global/neovim/config/lua/plugins/which-key.lua
+++ home-manager/users/simonwjackson/global/neovim/config/lua/plugins/which-key.lua
@@ -1,3 +1,4 @@
+--
 -- {
 --   "folke/which-key.nvim",
 --   event = "VeryLazy",
@@ -86,64 +87,6 @@ return {
 						vim.lsp.buf.hover()
 					end,
 					"LSP hover",
-				},
-
-				--           -- Navigation through hunks
-				["<Down>"] = {
-					function()
-						if vim.wo.diff then
-							return "]c"
-						end
-						vim.schedule(function()
-							require("gitsigns").next_hunk()
-						end)
-						return "<Ignore>"
-					end,
-					"Jump to next hunk",
-					opts = { expr = true },
-				},
-
-				["<Up>"] = {
-					function()
-						if vim.wo.diff then
-							return "[c"
-						end
-						vim.schedule(function()
-							require("gitsigns").prev_hunk()
-						end)
-						return "<Ignore>"
-					end,
-					"Jump to prev hunk",
-					opts = { expr = true },
-				},
-
-				-- Actions
-				["<leader>rh"] = {
-					function()
-						require("gitsigns").reset_hunk()
-					end,
-					"Reset hunk",
-				},
-
-				["<leader>ph"] = {
-					function()
-						require("gitsigns").preview_hunk()
-					end,
-					"Preview hunk",
-				},
-
-				["<leader>gb"] = {
-					function()
-						package.loaded.gitsigns.blame_line()
-					end,
-					"Blame line",
-				},
-
-				["<leader>td"] = {
-					function()
-						require("gitsigns").toggle_deleted()
-					end,
-					"Toggle deleted",
 				},
 
 				-- ["gi"] = {
