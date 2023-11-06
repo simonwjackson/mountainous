43d8583e2000c7e051d3bf82453c209a2e6b13bd
Simon W. Jackson <hello@simonwjackson.io>
2023-11-05 18:37:39 -0600

    :art: refactor(modules): reorder imports and expand work-mode configuration
    - Reordered the imports in `default.nix` for better organization
    - Expanded the configuration in `work-mode.nix` to include more settings for `windowManager.bspwm` and `home.file.".config/picom/config"`
    - Added a new `popup-term` in `work-mode.nix`

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -1,8 +1,8 @@
 {
-  work-mode = import ./work-mode.nix;
-  snowscape = import ./snowscape.nix;
   mpvd = import ./mpvd.nix;
   nest-tmux = import ./nest-tmux.nix;
+  snowscape = import ./snowscape.nix;
   vinyl-vault = import ./vinyl-vault.nix;
+  work-mode = import ./work-mode.nix;
   xpo = import ./xpo.nix;
 }
