059824d8d560bce93493bd8f29eccae45d4b7155
Simon W. Jackson <hello@simonwjackson.io>
2023-11-05 18:37:11 -0600

    :twisted_rightwards_arrows: refactor(pkgs): update package calls in default.nix
    - :heavy_minus_sign: Removed call to xpo and vinyl-vault packages
    - :heavy_plus_sign: Added calls to cpu-profile, ex, popup-term, vinyl-vault, wifi-select, and xpo packages
    - :arrows_counterclockwise: Reordered package calls for better organization

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -1,8 +1,11 @@
 {pkgs ? import <nixpkgs> {}}: rec {
-  xpo = pkgs.callPackage ./xpo {};
-  vinyl-vault = pkgs.callPackage ./vinyl-vault {};
-  herbstluftwm-scripts = pkgs.callPackage ./herbstluftwm-scripts {};
-  # ex = pkgs.callPackage ./ex { };
-  nest-tmux = pkgs.callPackage ./nest-tmux {};
   # mpvd = pkgs.callPackage ./mvpd {};
+  cpu-profile = pkgs.callPackage ./cpu-profile {};
+  ex = pkgs.callPackage ./ex {};
+  herbstluftwm-scripts = pkgs.callPackage ./herbstluftwm-scripts {};
+  nest-tmux = pkgs.callPackage ./nest-tmux {};
+  popup-term = pkgs.callPackage ./popup-term {};
+  vinyl-vault = pkgs.callPackage ./vinyl-vault {};
+  wifi-select = pkgs.callPackage ./wifi-select {};
+  xpo = pkgs.callPackage ./xpo {};
 }
