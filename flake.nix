0ed9678c76285b983fb5231c3267c3faf539c7ab
Simon W. Jackson <hello@simonwjackson.io>
2024-03-05 17:02:48 -0600

    refactor: update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # myflake.url = "path:/glacier/snowscape/code/sandbox/gaming-yl-database";
+    # inputs.myflake.nixosModules.x86_64-linux.default
+
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     nix-gaming.url = "github:fufexan/nix-gaming";
     cuttlefish.url = "https://flakehub.com/f/simonwjackson/cuttlefi.sh/*.tar.gz";
@@ -14,16 +18,7 @@
 
     # Nixpkgs
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-    nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     agenix.url = "github:ryantm/agenix";
     hardware.url = "github:nixos/nixos-hardware";
@@ -36,19 +31,14 @@
 
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/master";
     };
   };
 
   outputs = {
     agenix,
-    cuttlefish,
     hardware,
     home-manager,
-    nix-darwin,
-    nix-on-droid,
-    nix-flatpak,
-    nixos-generators,
     nixpkgs,
     self,
     ...
@@ -56,15 +46,27 @@
     inherit (self) outputs;
     lib = nixpkgs.lib // home-manager.lib;
     systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
-    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
     pkgsFor = lib.genAttrs systems (system:
       import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       });
     rootPath = ./.;
-    commonSpecialArgs = {inherit inputs outputs rootPath self;};
-    commonNixOsModules = [nix-flatpak.nixosModules.nix-flatpak];
+    mkSystem = hostName: system: extraModules:
+      lib.nixosSystem {
+        specialArgs = {inherit inputs outputs rootPath self;};
+        modules =
+          [
+            (./nixos/hosts + "/${hostName}")
+            agenix.nixosModules.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.users.simonwjackson = import (./home-manager/users/simonwjackson/hosts + "/${hostName}");
+              home-manager.extraSpecialArgs = {inherit inputs outputs rootPath self;};
+            }
+          ]
+          ++ extraModules;
+      };
   in {
     inherit lib;
 
@@ -81,161 +83,34 @@
 
     # Your custom packages
     # Acessible through 'nix build', 'nix shell', etc
-    packages = forEachSystem (pkgs: import ./nix/pkgs {inherit pkgs;});
+    packages = lib.genAttrs systems (system: import ./nix/pkgs {pkgs = pkgsFor.${system};});
 
-    devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
+    devShells = lib.genAttrs systems (system: import ./shell.nix {pkgs = pkgsFor.${system};});
 
     # Formatter for your nix files, available through 'nix fmt'
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    formatter = forEachSystem (pkgs: pkgs.alejandra);
+    formatter = lib.genAttrs systems (system: pkgsFor.${system}.alejandra);
 
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild <action> --flake .#your-hostname'
     nixosConfigurations = {
       # GPD Win Mini
-      kita = lib.nixosSystem {
-        specialArgs = commonSpecialArgs;
-        modules =
-          [
-            ./nixos/hosts/kita
-            agenix.nixosModules.default
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.users.simonwjackson = import ./home-manager/users/simonwjackson/hosts/kita;
-              home-manager.extraSpecialArgs = commonSpecialArgs;
-            }
-          ]
-          ++ commonNixOsModules;
-      };
+      kita = mkSystem "kita" "x86_64-linux" [];
 
       # Main laptop
-      fiji = lib.nixosSystem {
-        specialArgs = commonSpecialArgs;
-        modules = [
-          ./nixos/hosts/fiji
-          agenix.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.users.simonwjackson = import ./home-manager/users/simonwjackson/hosts/fiji;
-            home-manager.extraSpecialArgs = commonSpecialArgs;
-          }
-        ];
-      };
+      fiji = mkSystem "fiji" "x86_64-linux" [];
 
       # Remote Server
-      yabashi = lib.nixosSystem {
-        modules = [
-          ./nixos/hosts/yabashi
-          agenix.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.users.simonwjackson = import ./home-manager/users/simonwjackson/hosts/yabashi;
-            home-manager.extraSpecialArgs = commonSpecialArgs;
-          }
-        ];
-        specialArgs = commonSpecialArgs;
-      };
+      yabashi = mkSystem "yabashi" "x86_64-linux" [];
 
       # Home Server
-      unzen = lib.nixosSystem {
-        modules = [
-          ./nixos/hosts/unzen
-          agenix.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.users.simonwjackson = import ./home-manager/users/simonwjackson/hosts/unzen;
-            home-manager.extraSpecialArgs = commonSpecialArgs;
-          }
-        ];
-        specialArgs = commonSpecialArgs;
-      };
+      unzen = mkSystem "unzen" "x86_64-linux" [];
 
       # router
-      rakku = lib.nixosSystem {
-        modules = [
-          ./nixos/hosts/rakku
-          agenix.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.users.simonwjackson = import ./home-manager/users/simonwjackson/hosts/rakku;
-            home-manager.extraSpecialArgs = commonSpecialArgs;
-          }
-        ];
-        specialArgs = commonSpecialArgs;
-      };
+      rakku = mkSystem "rakku" "x86_64-linux" [];
 
       # portable gaming rig
-      zao = lib.nixosSystem {
-        modules = [
-          ./nixos/hosts/zao
-          agenix.nixosModules.default
-          hardware.nixosModules.dell-xps-17-9700-nvidia
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.users.simonwjackson = import ./home-manager/users/simonwjackson/hosts/zao;
-            home-manager.extraSpecialArgs = commonSpecialArgs;
-          }
-        ];
-        specialArgs = commonSpecialArgs;
-      };
-    };
-
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#ushiro
-    # INFO: Disabled
-    # darwinConfigurations."ushiro" = nix-darwin.lib.darwinSystem {
-    #   specialArgs = commonSpecialArgs;
-    #   modules = [
-    #     ./nix-darwin/hosts/ushiro
-    #     agenix.nixosModules.default
-    #     # home-manager.darwinModules.home-manager
-    #     # {
-    #     #   home-manager.extraSpecialArgs = commonSpecialArgs;
-    #     #   home-manager.users.sjackson217 = import ./home-manager/users/simonwjackson/hosts/ushiro;
-    #     # }
-    #   ];
-    # };
-
-    # Expose the package set, including overlays, for convenience.
-    # darwinPackages = self.darwinConfigurations."ushiro".pkgs;
-
-    # INFO: Disabled
-    # nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
-    #   extraSpecialArgs = commonSpecialArgs;
-    #   modules = [
-    #     ./nix-on-droid/hosts/usu
-    #     {
-    #       home-manager = {
-    #         backupFileExtension = "hm-bak";
-    #         extraSpecialArgs = commonSpecialArgs;
-    #         config = import ./home-manager/users/simonwjackson/hosts/usu;
-    #       };
-    #     }
-    #   ];
-    # };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager <action> --flake .#simonwjackson'
-    homeConfigurations = {
-      # Laptop
-      "simonwjackson@fiji" = lib.homeManagerConfiguration {
-        modules = [
-          ./home-manager/users/simonwjackson/hosts/fiji
-          # agenix.homeManagerModules.age
-        ];
-        pkgs = pkgsFor.x86_64-linux // outputs.packages;
-        extraSpecialArgs = commonSpecialArgs;
-      };
-
-      # Laptop
-      "sjackson217@ushiro" = lib.homeManagerConfiguration {
-        modules = [
-          ./home-manager/users/simonwjackson/hosts/ushiro
-          # agenix.homeManagerModules.age
-        ];
-        pkgs = pkgsFor.aarch64-darwin // outputs.packages;
-        extraSpecialArgs = commonSpecialArgs;
-      };
+      zao = mkSystem "zao" "x86_64-linux" [hardware.nixosModules.dell-xps-17-9700-nvidia];
     };
   };
 }
