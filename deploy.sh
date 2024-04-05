098b90830de66e8684f4ddbd10fd1b4e0c9f86e1
Simon W. Jackson <simon.jackson@gmail.com>
2024-04-05 11:32:01 -0500

    feat: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,23 +5,23 @@ hosts="$1"
 shift
 
 if [ -z "$hosts" ]; then
-  hosts="$(hostname)"
-else
   # Get the list of NixOS host names from flake.nix
   hosts=$(cat ./flake.nix | grep ' = mkSystem ' | awk -F ' ' '{print $1}' | awk '{printf $0 ",";}')
+# else
+#   hosts="$(hostname)"
 fi
 
 # Iterate over each host
 for host in ${hosts//,/ }; do
-  if nix store ping --store "ssh://$host" >/dev/null 2>&1; then
+  if nc -w 1 -z "${host}" 22 >/dev/null 2>&1; then
     nixos-rebuild \
       --flake ".#$host" \
       switch \
       --target-host "$host" \
       --use-remote-sudo \
       --use-substitutes \
-      "$@" \
-      --log-format internal-json -v |& nom --json
+      "$@"
+    # --log-format internal-json -v |& nom --json
   else
     offline_hosts+=("$host")
   fi
