cb3215fb5f229862ba16183f7127dcd25014f424
Simon W. Jackson <hello@simonwjackson.io>
2024-04-29 18:54:31 -0500

    feat: add kita

---
+++ modules/nixos/sound/default.nix
@@ -0,0 +1,14 @@
+{
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+    # lowLatency.enable = true;
+  };
+}
