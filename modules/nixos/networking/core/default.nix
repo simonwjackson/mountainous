0182fd1f3a37fed0887bf3380c46c718b9b94865
Simon W. Jackson <hello@simonwjackson.io>
2024-05-04 20:56:51 -0500

    feat: update network module

--- modules/nixos/networking/core/default.nix
+++ modules/nixos/networking/core/default.nix
@@ -2,6 +2,9 @@
   networking = {
     useDHCP = lib.mkDefault true;
     domain = "mountaino.us";
-    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+    networkmanager.enable = true;
   };
+
+  # WARN: This speeds up `nixos-rebuild`, but im not sure if there are any side effects
+  systemd.services.NetworkManager-wait-online.enable = false;
 }
