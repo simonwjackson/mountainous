e2760597ce2fefb8031d365295fcfbbd7f042d27
Simon W. Jackson <hello@simonwjackson.io>
2024-05-08 09:53:55 -0500

    feat: convert taskwarrior into optional nixos module

--- modules/home/taskwarrior/default.nix
+++ modules/home/taskwarrior/default.nix
@@ -1,50 +1,66 @@
-{config, ...}: {
-  age.secrets."user-simonwjackson-taskserver-private.key".file = ../../../secrets/user-simonwjackson-taskserver-private.key.age;
-  age.secrets."user-simonwjackson-taskserver-public.cert".file = ../../../secrets/user-simonwjackson-taskserver-public.cert.age;
-  age.secrets."user-simonwjackson-taskserver-ca.cert".file = ../../../secrets/user-simonwjackson-taskserver-ca.cert.age;
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib) mkEnableOption;
 
-  programs.taskwarrior = {
-    enable = true;
-    config = {
-      confirmation = false;
-      taskd = {
-        certificate = config.age.secrets."user-simonwjackson-taskserver-public.cert".path;
-        key = config.age.secrets."user-simonwjackson-taskserver-private.key".path;
-        ca = config.age.secrets."user-simonwjackson-taskserver-ca.cert".path;
-        server = "yari:53589";
-        credentials = "mountainous/simonwjackson/430e9d17-bc5e-4534-9c37-c1dcab337dbe";
+  cfg = config.mountainous.taskwarrior-sync;
+in {
+  options.mountainous.taskwarrior-sync = {
+    enable = mkEnableOption "Whether to enable the service";
+  };
+
+  config = lib.mkIf cfg.enable {
+    # Add your module configuration here
+    age.secrets."user-simonwjackson-taskserver-private.key".file = ../../../secrets/user-simonwjackson-taskserver-private.key.age;
+    age.secrets."user-simonwjackson-taskserver-public.cert".file = ../../../secrets/user-simonwjackson-taskserver-public.cert.age;
+    age.secrets."user-simonwjackson-taskserver-ca.cert".file = ../../../secrets/user-simonwjackson-taskserver-ca.cert.age;
+
+    programs.taskwarrior = {
+      enable = true;
+      config = {
+        confirmation = false;
+        taskd = {
+          certificate = config.age.secrets."user-simonwjackson-taskserver-public.cert".path;
+          key = config.age.secrets."user-simonwjackson-taskserver-private.key".path;
+          ca = config.age.secrets."user-simonwjackson-taskserver-ca.cert".path;
+          server = "yari:53589";
+          credentials = "mountainous/simonwjackson/430e9d17-bc5e-4534-9c37-c1dcab337dbe";
+        };
       };
     };
-  };
 
-  home.file.".local/share/task/hooks/on-exit-sync.sh" = {
-    # TODO: move this to an external file
-    text = ''
-      #!/bin/sh
-      # This hooks script syncs task warrior to the configured task server.
-      # The on-exit event is triggered once, after all processing is complete.
+    home.file.".local/share/task/hooks/on-exit-sync.sh" = {
+      # TODO: move this to an external file
+      text = ''
+        #!/bin/sh
+        # This hooks script syncs task warrior to the configured task server.
+        # The on-exit event is triggered once, after all processing is complete.
 
-      # Make sure hooks are enabled
+        # Make sure hooks are enabled
 
 
-      # Count the number of tasks modified
-      n=0
-      while read modified_task
-      do
-          n=$(($n + 1))
-      done
+        # Count the number of tasks modified
+        n=0
+        while read modified_task
+        do
+            n=$(($n + 1))
+        done
 
-      if (($n > 0)); then
-          task sync >> ~/sync_hook.log > /dev/null 2>&1 &
-      fi
+        if (($n > 0)); then
+            task sync >> ~/sync_hook.log > /dev/null 2>&1 &
+        fi
 
-      exit 0
-    '';
-    executable = true;
-  };
+        exit 0
+      '';
+      executable = true;
+    };
 
-  services.taskwarrior-sync = {
-    enable = true;
-    frequency = "*:0/5";
+    services.taskwarrior-sync = {
+      enable = true;
+      frequency = "*:0/5";
+    };
   };
 }
