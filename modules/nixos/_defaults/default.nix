191231fb47e98a845573511fde7c0d6afbde7eb9
Simon W. Jackson <hello@simonwjackson.io>
2024-06-12 21:39:40 -0500

    chore: update sane defaults

--- modules/nixos/_defaults/default.nix
+++ modules/nixos/_defaults/default.nix
@@ -1,22 +1,23 @@
 # This file (and the global directory) holds config that i use on all hosts
 {
-  system,
-  options,
-  lib,
-  inputs,
-  pkgs,
   config,
+  host,
+  inputs,
+  lib,
+  options,
+  pkgs,
+  system,
   ...
 }: let
   inherit (lib.snowfall.fs) get-file;
-  inherit (lib) mkDefault;
+  inherit (lib) mkIf mkDefault;
   inherit (lib.mountainous) enabled disabled;
   inherit (lib.mountainous.syncthing) otherDevices;
 in {
   programs.icho = {
     enable = true;
     environment = {
-      NOTES_DIR = "/glacier/snowscape/notes";
+      NOTES_DIR = mkDefault "/glacier/snowscape/notes";
     };
     environmentFiles = [
       config.age.secrets."user-simonwjackson-anthropic".path
@@ -26,14 +27,10 @@ in {
   programs.zsh.enable = true;
 
   backpacker = {
-    adb = {
-      enable = mkDefault true;
-      user = "simonwjackson";
-    };
+    adb = mkDefault enabled;
     agenix = {
       enable = true;
       secretsDir = get-file "secrets";
-      user = "simonwjackson";
     };
     boot = mkDefault enabled;
     hardware = {
@@ -56,27 +53,25 @@ in {
     printing = mkDefault enabled;
     profiles = {
       laptop = mkDefault disabled;
-      workspace = {
-        enable = mkDefault false;
-        user = "simonwjackson";
-      };
+      workspace = mkDefault disabled;
     };
-    security = {
-      enable = mkDefault true;
-      user = "simonwjackson";
-    };
+    security = mkDefault enabled;
     sound = mkDefault enabled;
-    syncthing = {
+    syncthing = rec {
       inherit otherDevices;
+
+      cert = mkIf config.backpacker.syncthing.enable (mkDefault config.age.secrets."${host}-syncthing-cert".path);
       enable = mkDefault true;
+      hostName = host;
+      key = mkIf config.backpacker.syncthing.enable (mkDefault config.age.secrets."${host}-syncthing-key".path);
       systemsDir = get-file "systems";
-      hostName = config.networking.hostName;
-      user = "simonwjackson";
     };
     user = {
       enable = mkDefault true;
       name = mkDefault "simonwjackson";
       hashedPasswordFile = mkDefault config.age.secrets."user-simonwjackson".path;
+      # authorizedKeys = [builtins.readFile ../../../rsa.pub];
+
       authorizedKeys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/PwyhdbVKd6jcG55m/1sUgEf0x3LUeS9H4EK5vk9PKhvDsjOQOISyR1LBmmXUFamkpFo2c84ZgPMj33qaPfOF0VfmF79vdAIDdDt5bmsTU6IbT7tGJ1ocpHDqhqbDO3693RdbTt1jTQN/eo3AKOfnrMouwBZPbPVqoWEhrLUvUTuTq7VQ+lUqWkvGs4D6D8UeIlG9VVgVhad3gCohYsjGdzgOUy0V4c8t3BuHrIE6//+6YVJ9VWK/ImSWmN8it5RIREDgdSYujs1Uod+ovr8AvaGFlFC9GuYMsj7xDYL1TgaWhy5ojk6JcuuF0cmoqffoW/apYdYM6Vxi5Xe6aJUhVyguZDovWcqRdPv2q0xtZn6xvNkoElEkrb6t0CAbGKf++H4h8/v5MsMt9wUPJAJBa24v0MlU8mXTUwhFLP5YQ/A8AAb5Y3ty/6DaOlvvTzt5Om2SMrZ1XaL1II35dFNZ/Os3zRpqdWq9SnpisRA+Bpf0bPUjdi8D8rRJn8g3zO5EsldBlZg82PiJcRHANbydTSK6Jzw7A8S5gMyPoH80Pq5MbQPvPpevTfOKy14NyTYPHGj0j5y7EQP7yb6w70LtqdRLRLQSTCdF0qTjVWw/qdt9MXkS7cdQe4yBADmjwozwPuxAs/jNpxELcVPEWBK6DcAIFD0vv3Xaw7reXpXFTQ==" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDlS7mK1MSLJviO83iAxwE5FQOu6FU9IeY6qcj6qYZ1s8qevcgj94CKhLq/ud/TexZ3qWVHkidmX0idQ4eo10lCYhAMynxT4YbtXDvHzWeeAYVN9JGyBdl4+HNctzdIKDrdOZzu+MBKgXjshuSntMUIabe7Bes+5B75ppwWqANFNPMKUSqTENxvmZ6mHF+KdwOI1oXYvOHD5y3t1dtWWcLMrot6F/ZUae5L7sRp+PqykOV4snI06uTeUxs0cTZJULDwNgngqIG9qs72BCfVvuOOwYosezUoajikPzzbBOJBl6l3M7MSJQfilVgvT/gHAxJKuZ1RzrPrssYBCbVanEL6dXuhiI25yxQvIqxDJmLzI9hvVwGgJJzov9BduO+vvPX/AwMd1oLxScgISkK/y+6+VHz+ey88gVniw22mSG0ueG11eebtp9c/lmBpNxZ30gmaINbgxZn4sM99RtC3E8eJ+KmKet8L+tFtVdeCYB7pgk8k/h06s9s3r34TGJ+SmrU="];
     };
     vpn-proxy = mkDefault disabled;
@@ -87,7 +82,7 @@ in {
   # services.udisks2.enable = true;
 
   # TODO: Move to (desktop?) profile
-  environment.variables.BROWSER = "firefox-esr";
+  environment.variables.BROWSER = "firefox";
 
   programs.tmesh = let
     systems = lib.snowfall.fs.get-file "systems";
