85df79542e9fd5f11adeb8766bbb12c07c26284e
Simon W. Jackson <189379+simonwjackson@users.noreply.github.com>
2024-09-12 19:16:48 -0500

    fix: add fsid

--- systems/x86_64-linux/aka/default.nix
+++ systems/x86_64-linux/aka/default.nix
@@ -38,9 +38,9 @@ in {
   services.nfs.server = {
     enable = true;
     exports = ''
-      /snowscape 192.168.1.0/24(rw,sync,no_subtree_check) \
-                 100.64.0.0/10(rw,sync,no_subtree_check) \
-                 172.16.0.0/12(rw,sync,no_subtree_check)
+      /snowscape 192.168.1.0/24(rw,sync,no_subtree_check,fsid=0) \
+                 100.64.0.0/10(rw,sync,no_subtree_check,fsid=0) \
+                 172.16.0.0/12(rw,sync,no_subtree_check,fsid=0)
     '';
   };
 
