0b8b2b7d4c9a3573f3bc9719ea4f944ce527e7db
Simon W. Jackson <189379+simonwjackson@users.noreply.github.com>
2024-11-19 15:59:05 -0600

    feat(unzen): add second parity disk

--- systems/x86_64-linux/unzen/disko/snapraid.nix
+++ systems/x86_64-linux/unzen/disko/snapraid.nix
@@ -1,12 +1,36 @@
 {...}: {
   disko.devices.disk = {
-    parity00 = {
+    hdd00 = {
       type = "disk";
       device = "/dev/disk/by-id/ata-WDC_WD80EFAX-68LHPN0_7SGKDA3C";
       content = {
         type = "gpt";
         partitions = {
           parity00 = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "xfs";
+              mountpoint = "/data/parity/00";
+              mountOptions = [
+                "defaults"
+                "nofail"
+                "noatime"
+                "logbufs=8"
+              ];
+            };
+          };
+        };
+      };
+    };
+
+    hdd01 = {
+      type = "disk";
+      device = "/dev/disk/by-id/ata-WDC_WD80EFBX-68AZZN0_VRJVWS3K";
+      content = {
+        type = "gpt";
+        partitions = {
+          parity01 = {
             size = "100%";
             content = {
               type = "filesystem";
