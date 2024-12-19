7c55f41d38f3055017594342f4274aaab15fd788
Simon W. Jackson <github@simonwjackson.io>
2024-12-18 20:58:00 -0600

    feat: add code to syncthing

--- systems/x86_64-linux/fuji/disko.nix
+++ systems/x86_64-linux/fuji/disko.nix
@@ -1,3 +1,5 @@
+# TODO: Yubi key
+# https://haseebmajid.dev/posts/2024-07-30-how-i-setup-btrfs-and-luks-on-nixos-using-disko/
 {
   disko.devices = {
     disk = {
@@ -21,28 +23,27 @@
               content = {
                 type = "luks";
                 name = "crypted";
+                # extraOpenArgs = [
+                #   "--allow-discards"
+                #   "--perf-no_read_workqueue"
+                #   "--perf-no_write_workqueue"
+                # ];
+                # settings = {crypttabExtraOpts = ["fido2-device=auto" "token-timeout=10"];};
                 content = {
                   type = "btrfs";
                   extraArgs = ["-f"];
+                  # extraArgs = ["-L" "nixos" "-f"];
                   subvolumes = {
                     "/nix" = {
                       mountpoint = "/nix";
                       mountOptions = ["compress=zstd" "noatime"];
                     };
-                    "/persist/home" = {
-                      mountpoint = "/home";
-                      mountOptions = ["compress=zstd" "noatime"];
-                    };
+                    # "/log" = {
+                    #   mountpoint = "/var/log";
+                    #   mountOptions = ["subvol=log" "compress=zstd" "noatime"];
+                    # };
                     "/persist/system" = {
                       mountpoint = "/persist";
-                      mountOptions = ["compress=zstd" "noatime"];
-                    };
-                    "/persist/blizzard.0.00" = {
-                      mountpoint = "/avalanche/volumes/blizzard";
-                      mountOptions = ["compress=zstd" "noatime"];
-                    };
-                    "/persist/log" = {
-                      mountpoint = "/var/log";
                       mountOptions = ["compress=zstd" "noatime"];
                     };
                   };
--- systems/x86_64-linux/fuji/syncthing.nix
+++ systems/x86_64-linux/fuji/syncthing.nix
@@ -8,8 +8,115 @@
     name = "Laptop (${host})";
   };
   shares = {
+    code = {
+      path = "/snowscape/code";
+      type = "sendreceive";
+      # TODO: Make this "global"
+      blacklist = [
+        # Node.js
+        "node_modules/"
+
+        # Build outputs
+        "dist/"
+        "build/"
+        "out/"
+        ".next/"
+        ".nuxt/"
+        ".output/"
+
+        # Environment and config
+        ".env.local"
+        ".env.*.local"
+        "config.local.js"
+        "*.local.json"
+
+        # Dependencies and package managers
+        "vendor/"
+        "composer.phar"
+        "Pipfile.lock"
+        "poetry.lock"
+        "package-lock.json"
+        "yarn.lock"
+        "pnpm-lock.yaml"
+
+        # IDE and editor files
+        ".idea/"
+        "*.swp"
+        "*.swo"
+        ".project"
+        ".settings/"
+        ".classpath"
+        ".factorypath"
+
+        # Testing and coverage
+        "coverage/"
+        ".nyc_output/"
+        ".coverage"
+        "htmlcov/"
+        ".pytest_cache/"
+        "__pycache__/"
+        "*.py[cod]"
+        ".tox/"
+
+        # Logs and databases
+        "*.log"
+        "*.sqlite"
+        "*.sqlite3"
+        "*.db"
+        "logs/"
+        "*.log.*"
+
+        # Temporary files
+        "tmp/"
+        "temp/"
+        ".temp/"
+        "*.tmp"
+        "*.bak"
+        "*.swp"
+        "*.swo"
+
+        # Binary and compiled files
+        "*.class"
+        "*.dll"
+        "*.exe"
+        "*.o"
+        "*.so"
+        "*.dylib"
+        "*.jar"
+        "*.war"
+        "*.ear"
+        "*.zip"
+        "*.tar.gz"
+        "*.rar"
+
+        # Mobile development
+        ".gradle/"
+        "*.apk"
+        "*.aab"
+        "*.ipa"
+        "*.dSYM.zip"
+        "*.dSYM"
+
+        "# Documentation"
+        "docs/_build/"
+        "site/"
+
+        # Cache
+        ".cache/"
+        ".parcel-cache/"
+        ".eslintcache"
+        ".stylelintcache"
+        "*.cache"
+
+        # Misc
+        "Thumbs.db"
+        "ehthumbs.db"
+        "*~"
+        ".DS_Store"
+      ];
+    };
     notes = {
-      path = "/glacier/snowscape/notes";
+      path = "/snowscape/notes";
       type = "sendreceive";
     };
   };
