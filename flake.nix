26273fe4d719b9c45a0b84d953b5c48d1dd2bcb9
Simon W. Jackson <189379+simonwjackson@users.noreply.github.com>
2025-04-10 14:09:08 -0500

    chore: refactor system configs to use architecture-based directory structure

--- flake.nix
+++ flake.nix
@@ -9,29 +9,46 @@
     self,
     nixpkgs,
     ...
-  } @ inputs: {
-    nixosConfigurations = {
-      # First system configuration
-      system1 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration-system1.nix
-        ];
-      };
-
-      # Second system configuration
-      system2 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration-system2.nix
-        ];
-      };
+  } @ inputs: let
+    # Get all architectures from the systems directory
+    architectures = builtins.attrNames (builtins.readDir ./systems);
+    
+    # Function to create a nixosSystem for a given architecture and system name
+    mkNixosSystem = arch: name: nixpkgs.lib.nixosSystem {
+      system = arch;
+      modules = [
+        ./systems/${arch}/${name}/default.nix
+      ];
     };
+    
+    # Function to build system configurations for a specific architecture
+    systemsForArch = arch: 
+      let 
+        systemNames = builtins.attrNames (builtins.readDir ./systems/${arch});
+        systems = builtins.listToAttrs (map (name: {
+          inherit name;
+          value = mkNixosSystem arch name;
+        }) systemNames);
+      in systems;
+    
+    # Combine all system configurations across all architectures
+    allSystems = builtins.foldl' (acc: arch: 
+      acc // (systemsForArch arch)
+    ) {} architectures;
+    
+    # Collect all VM builds for each architecture
+    mkVmPackages = arch: 
+      let
+        systemNames = builtins.attrNames (builtins.readDir ./systems/${arch});
+        vmPkgs = builtins.listToAttrs (map (name: {
+          name = "vm-${name}";
+          value = self.nixosConfigurations.${name}.config.system.build.vm;
+        }) systemNames);
+      in vmPkgs;
+  in {
+    nixosConfigurations = allSystems;
 
-    # VM outputs for both configurations
-    packages.x86_64-linux = {
-      vm-system1 = self.nixosConfigurations.system1.config.system.build.vm;
-      vm-system2 = self.nixosConfigurations.system2.config.system.build.vm;
-    };
+    # VM outputs for all configurations
+    packages.x86_64-linux = mkVmPackages "x86_64-linux";
   };
 }
