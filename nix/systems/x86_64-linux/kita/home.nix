2e7f4dec330ecb66ab47eb482493499c78e8fa63
NixOS User <user@example.com>
2025-04-13 14:56:07 +0000

    feat: add Nreal display configuration to kanshi monitor profiles

--- nix/systems/x86_64-linux/kita/home.nix
+++ nix/systems/x86_64-linux/kita/home.nix
@@ -29,6 +29,23 @@
           }
         ];
       };
+
+      nreal = {
+        exec = [
+          "${pkgs.hyprland}/bin/hyprctl --instance 0 keyword monitor eDP-1,disabled && ${pkgs.hyprland}/bin/hyprctl --instance 0 keyword monitor desc:Nreal nreal air 0x88888800,1920x1080@60,auto,1.25 && ${pkgs.coreutils}/bin/sleep 0.5 && ${pkgs.hyprland}/bin/hyprctl --instance 0 dispatch workspace 2"
+        ];
+        outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+          {
+            criteria = "Nreal nreal air 0x88888800";
+            status = "enable";
+          }
+        ];
+      };
+
       docked = {
         exec = [
           "${pkgs.hyprland}/bin/hyprctl --instance 0 keyword monitor eDP-1,disabled && ${pkgs.hyprland}/bin/hyprctl --instance 0 keyword monitor DP-6,2880x1800@120,0x1200,1.5 && ${pkgs.hyprland}/bin/hyprctl --instance 0 keyword monitor DP-7,2880x1800@120,0x0,1.5 && ${pkgs.hyprland}/bin/hyprctl --instance 0 dispatch workspace 1"
@@ -39,6 +56,7 @@
             status = "disable";
           }
           {
+            # INFO: Using DP-6 and DP-7 as "unique" identifiers since they are identical devices
             criteria = "DP-6";
             status = "enable";
           }
