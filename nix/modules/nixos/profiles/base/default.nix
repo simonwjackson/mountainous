4e63a5852cc0facb01fb386f25207144a0807b04
NixOS User <user@example.com>
2025-04-14 21:14:49 -0500

    feat: add performance tuning and update nix settings in base profile

--- nix/modules/nixos/profiles/base/default.nix
+++ nix/modules/nixos/profiles/base/default.nix
@@ -3,6 +3,7 @@
   config,
   lib,
   pkgs,
+  inputs,
   ...
 }: let
   inherit (lib) mkIf mkDefault mkEnableOption;
@@ -68,23 +69,19 @@ in {
     # TODO: Move to (desktop?) profile
     environment.variables.BROWSER = "firefox";
 
-    services.gpm.enable = true; # TTY mouse
+    # Enable TTY mouse
+    services.gpm.enable = true;
 
     # This is a hack to get around a bug in nixos-option
     # TODO: Remove this when nixos-option is fixed
     # INFO: https://github.com/NixOS/nixpkgs/issues/291051
     environment.etc."NIXOS_OZONE_WL".text = "1";
 
-    # Enable flakes
-    nix.settings.experimental-features = ["nix-command" "flakes"];
-  
     hardware = {
+      # This means we can use firmware that is not redistributable
       enableRedistributableFirmware = true;
       enableAllFirmware = true;
     };
-
-    # Enable SSH
-    services.openssh.enable = true;
 
     # Network configuration
     networking = {
@@ -94,5 +91,73 @@ in {
     # Time zone and locale
     time.timeZone = "America/Chicago";
     i18n.defaultLocale = "en_US.UTF-8";
+
+    services = {
+      openssh.enable = true;
+
+      # A daemon that automatically adjusts CPU frequency based on usage
+      auto-cpufreq.enable = true;
+
+      # A shell daemon created to manage processes' IO and CPU priorities, with community-driven set of rules
+      ananicy = {
+        enable = true;
+        package = pkgs.ananicy-cpp;
+      };
+    };
+
+    # A compiler cache that can speed up build times
+    programs.ccache.enable = true;
+
+    nixpkgs = {
+      config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [
+          "python-2.7.18.6"
+        ];
+      };
+    };
+
+    nix = {
+      package = pkgs.nixVersions.latest;
+      # This will add each flake input as a registry
+      # To make nix3 commands consistent with your flake
+      registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+
+      # This will additionally add your inputs to the system's legacy channels
+      # Making legacy nix commands consistent as well, awesome!
+      nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+      optimise.automatic = true;
+      settings = {
+        flake-registry = ""; # Disable global flake registry
+        warn-dirty = false;
+        # Enable flakes
+        experimental-features = ["nix-command" "flakes"];
+        # Add cachix binary cache
+        trusted-substituters = [
+          "https://nix-gaming.cachix.org"
+          "https://cache.nixos.org/"
+          "https://nix-community.cachix.org"
+          "https://simonwjackson.cachix.org"
+          "https://hyprland.cachix.org"
+        ];
+        trusted-users = ["root" "@wheel" "simonwjackson" "admin"];
+        trusted-public-keys = [
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "simonwjackson.cachix.org-1:MtG0AE8J6bjFO/wD04X5h8MlQh7Sbee8KAJrAsPJydI="
+          "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        ];
+        auto-optimise-store = true;
+      };
+
+      distributedBuilds = true;
+      extraOptions = ''
+        builders-use-substitutes = true
+      '';
+      # TODO: Add age secrets
+      # !include ${config.age.secrets."user-simonwjackson-github-token-nix".path};
+    };
   };
 }
