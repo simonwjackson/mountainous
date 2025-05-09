21229281018667fa7dd0203379982e870f0d66c8
Simon W. Jackson <github@simonwjackson.io>
2025-05-09 14:16:53 -0500

    feat: add lazygit

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -9,6 +9,288 @@
     enableBashIntegration = true;
     enableZshIntegration = true;
   };
+
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui = {
+        tabWidth = 2;
+        skipNoStagedFilesWarning = true;
+        sidePanelWidth = 0.381;
+        expandFocusedSidePanel = true;
+        expandedSidePanelWeight = 5;
+        timeFormat = "Jan 02, 06";
+        shortTimeFormat = "3:04pm";
+        theme = {
+          activeBorderColor = [
+            "#ca9ee6"
+            "bold"
+          ];
+          inactiveBorderColor = [
+            "#626880"
+          ];
+          searchingActiveBorderColor = [
+            "#ef9f76"
+            "bold"
+          ];
+          optionsTextColor = [
+            "#ca9ee6"
+          ];
+          selectedLineBgColor = [
+            "#51576d"
+          ];
+          inactiveViewSelectedLineBgColor = [
+            "#414559"
+          ];
+          cherryPickedCommitFgColor = [
+            "#ea999c"
+          ];
+          cherryPickedCommitBgColor = [
+            "#eebebe"
+          ];
+          markedBaseCommitFgColor = [
+            "#ca9ee6"
+          ];
+          markedBaseCommitBgColor = [
+            "#ef9f76"
+          ];
+          unstagedChangesColor = [
+            "#e78284"
+          ];
+          defaultFgColor = [
+            "#c6d0f5"
+          ];
+        };
+        showListFooter = false;
+        showRandomTip = false;
+        showCommandLog = false;
+        showBottomLine = true;
+        showPanelJumps = false;
+        nerdFontsVersion = "3";
+        showFileIcons = true;
+        spinner = {
+          rate = 50;
+          frames = [
+            "⠋"
+            "⠙"
+            "⠹"
+            "⠸"
+            "⠼"
+            "⠴"
+            "⠦"
+            "⠧"
+            "⠇"
+            "⠏"
+          ];
+        };
+      };
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "BAT_THEME=\"Catppuccin Frappe\" nix run nixpkgs#delta -- --paging=never";
+          useConfig = false;
+        };
+        parseEmoji = true;
+      };
+      os = {
+        edit = "nvr --remote-tab-silent {{filename}}";
+        editAtLine = "";
+        editAtLineAndWait = "";
+        editInTerminal = true;
+        openDirInEditor = "nvr --remote-tab-silent {{filename}}";
+        editPreset = "";
+        open = "nvr --remote-tab-silent {{filename}}";
+        copyToClipboardCmd = "wl-copy";
+        readFromClipboardCmd = "wl-paste";
+        shellFunctionsFile = "";
+      };
+      keybinding = {
+        universal = {
+          quit = "q";
+          quit-alt1 = "<c-c>";
+          return = "<esc>";
+          quitWithoutChangingDirectory = "Q";
+          togglePanel = "<tab>";
+          prevItem = "<up>";
+          nextItem = "<down>";
+          prevItem-alt = "k";
+          nextItem-alt = "j";
+          prevPage = ",";
+          nextPage = ".";
+          scrollLeft = "H";
+          scrollRight = "L";
+          gotoTop = "<";
+          gotoBottom = ">";
+          gotoTop-alt = "<home>";
+          gotoBottom-alt = "<end>";
+          toggleRangeSelect = "v";
+          rangeSelectDown = "<s-down>";
+          rangeSelectUp = "<s-up>";
+          prevBlock = "<left>";
+          nextBlock = "<right>";
+          prevBlock-alt = "h";
+          nextBlock-alt = "l";
+          nextBlock-alt2 = "<tab>";
+          prevBlock-alt2 = "<backtab>";
+          jumpToBlock = [
+            "1"
+            "2"
+            "3"
+            "4"
+            "5"
+          ];
+          focusMainView = "0";
+          nextMatch = "n";
+          prevMatch = "N";
+          startSearch = "/";
+          optionMenu = "<disabled>";
+          optionMenu-alt1 = "?";
+          select = "<space>";
+          goInto = "<enter>";
+          confirm = "<enter>";
+          confirmInEditor = "<a-enter>";
+          remove = "d";
+          new = "n";
+          edit = "e";
+          openFile = "o";
+          scrollUpMain = "<pgup>";
+          scrollDownMain = "<pgdown>";
+          scrollUpMain-alt1 = "K";
+          scrollDownMain-alt1 = "J";
+          scrollUpMain-alt2 = "<c-u>";
+          scrollDownMain-alt2 = "<c-d>";
+          executeShellCommand = ":";
+          createRebaseOptionsMenu = "m";
+          pushFiles = "P";
+          pullFiles = "p";
+          refresh = "R";
+          createPatchOptionsMenu = "<c-p>";
+          nextTab = "]";
+          prevTab = "[";
+          nextScreenMode = "+";
+          prevScreenMode = "_";
+          undo = "z";
+          redo = "<c-z>";
+          filteringMenu = "<c-s>";
+          diffingMenu = "W";
+          diffingMenu-alt = "<c-e>";
+          copyToClipboard = "<c-o>";
+          openRecentRepos = "<c-r>";
+          submitEditorText = "<enter>";
+          extrasMenu = "@";
+          toggleWhitespaceInDiffView = "<c-w>";
+          increaseContextInDiffView = "}";
+          decreaseContextInDiffView = "{";
+          increaseRenameSimilarityThreshold = ")";
+          decreaseRenameSimilarityThreshold = "(";
+          openDiffTool = "<c-t>";
+        };
+        status = {
+          checkForUpdate = "u";
+          recentRepos = "<enter>";
+          allBranchesLogGraph = "a";
+        };
+        files = {
+          commitChanges = "c";
+          commitChangesWithoutHook = "w";
+          amendLastCommit = "A";
+          commitChangesWithEditor = "C";
+          findBaseCommitForFixup = "<c-f>";
+          confirmDiscard = "x";
+          ignoreFile = "i";
+          refreshFiles = "r";
+          stashAllChanges = "s";
+          viewStashOptions = "S";
+          toggleStagedAll = "a";
+          viewResetOptions = "D";
+          fetch = "f";
+          toggleTreeView = "`";
+          openMergeTool = "M";
+          openStatusFilter = "<c-b>";
+          copyFileInfoToClipboard = "y";
+          collapseAll = "-";
+          expandAll = "=";
+        };
+        branches = {
+          createPullRequest = "o";
+          viewPullRequestOptions = "O";
+          copyPullRequestURL = "<c-y>";
+          checkoutBranchByName = "c";
+          forceCheckoutBranch = "F";
+          rebaseBranch = "r";
+          renameBranch = "R";
+          mergeIntoCurrentBranch = "M";
+          moveCommitsToNewBranch = "N";
+          viewGitFlowOptions = "i";
+          fastForward = "f";
+          createTag = "T";
+          pushTag = "P";
+          setUpstream = "u";
+          fetchRemote = "f";
+          sortOrder = "s";
+        };
+        worktrees = {
+          viewWorktreeOptions = "w";
+        };
+        commits = {
+          squashDown = "s";
+          renameCommit = "r";
+          renameCommitWithEditor = "R";
+          viewResetOptions = "g";
+          markCommitAsFixup = "f";
+          createFixupCommit = "F";
+          squashAboveCommits = "S";
+          moveDownCommit = "<c-j>";
+          moveUpCommit = "<c-k>";
+          amendToCommit = "A";
+          resetCommitAuthor = "a";
+          pickCommit = "p";
+          revertCommit = "t";
+          cherryPickCopy = "C";
+          pasteCommits = "V";
+          markCommitAsBaseForRebase = "B";
+          tagCommit = "T";
+          checkoutCommit = "<space>";
+          resetCherryPick = "<c-R>";
+          copyCommitAttributeToClipboard = "y";
+          openLogMenu = "<c-l>";
+          openInBrowser = "o";
+          viewBisectOptions = "b";
+          startInteractiveRebase = "i";
+          selectCommitsOfCurrentBranch = "*";
+        };
+        amendAttribute = {
+          resetAuthor = "a";
+          setAuthor = "A";
+          addCoAuthor = "c";
+        };
+        stash = {
+          popStash = "g";
+          renameStash = "r";
+        };
+        commitFiles = {
+          checkoutCommitFile = "c";
+        };
+        main = {
+          toggleSelectHunk = "a";
+          pickBothHunks = "b";
+          editSelectHunk = "E";
+        };
+        submodules = {
+          init = "i";
+          update = "u";
+          bulkMenu = "b";
+        };
+        commitMessage = {
+          commitMenu = "<c-o>";
+        };
+      };
+      quitOnTopLevelReturn = true;
+      disableStartupPopups = true;
+      customCommands = [];
+      promptToReturnFromSubprocess = false;
+    };
+  };
   programs.bat.enable = true;
   programs.lf = {
     enable = true;
@@ -24,6 +306,7 @@
       colors = true;
       incsearch = true;
       scrolloff = 10;
+      hidden = true;
     };
 
     # Key bindings
@@ -42,10 +325,12 @@
       "J" = "move-parent down";
       "K" = "move-parent up";
       "p" = "paste; clear";
+      "~" = "cd ~";
     };
 
     commands = let
       cp = "${pkgs.coreutils}/bin/cp";
+      cat = "${pkgs.coreutils}/bin/cat";
       dirname = "${pkgs.coreutils}/bin/dirname";
       find = "${pkgs.findutils}/bin/find";
       fzf = lib.getExe pkgs.fzf;
@@ -53,7 +338,6 @@
       mkdir = "${pkgs.coreutils}/bin/mkdir";
       mv = "${pkgs.coreutils}/bin/mv";
       ouch = lib.getExe pkgs.ouch;
-      p7z = lib.getExe pkgs.p7zip;
       printf = "${pkgs.coreutils}/bin/printf";
       rm = "${pkgs.coreutils}/bin/rm";
       rsync = lib.getExe pkgs.rsync;
@@ -62,8 +346,6 @@
       tar = lib.getExe pkgs.gnutar;
       touch = "${pkgs.coreutils}/bin/touch";
       tr = "${pkgs.coreutils}/bin/tr";
-      unrar = lib.getExe pkgs.unrar;
-      unzip = lib.getExe pkgs.unzip;
       zip = lib.getExe pkgs.zip;
       zoxide = lib.getExe pkgs.zoxide;
     in {
@@ -117,7 +399,7 @@
       '';
       paste = ''
         &{{
-          set -- $(cat ~/.local/share/lf/files)
+          set -- $(${cat} ~/.local/share/lf/files)
           mode="$1"
           shift
           case "$mode" in
