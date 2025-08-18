ff5904a5c4b0abfb4a61714088767b4145a26351
Simon W. Jackson <github@simonwjackson.io>
2025-08-18 16:04:13 -0500

    fix(hyprland): improve screenshot toggle logic

--- nix/modules/home/hyprland/default.nix
+++ nix/modules/home/hyprland/default.nix
@@ -306,7 +306,7 @@ in {
           "$mainMod, right, swapwindow, r"
           "$mainMod, up, swapwindow, u"
           "$mainMod, down, swapwindow, d"
-          "$mainMod, S, exec, pkill -f 'hyprshot.*ksnip' || (${pkgs.hyprshot}/bin/hyprshot --freeze --mode=region --raw | ${pkgs.ksnip}/bin/ksnip -)"
+          "$mainMod, S, exec, pgrep -f 'hyprshot.*ksnip' > /dev/null && pkill -f 'hyprshot.*ksnip' || (${pkgs.hyprshot}/bin/hyprshot --freeze --mode=region --raw | ${pkgs.ksnip}/bin/ksnip -)"
           "$mainMod SHIFT, S, movetoworkspace, special:magic"
           "$mainMod, N, exec, ${pkgs.darkmode-toggle}/bin/darkmode-toggle"
           "$mainMod, equal, exec, ${splitToggleScript}/bin/splitToggle"
