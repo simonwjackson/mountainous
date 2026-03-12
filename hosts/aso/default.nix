95fef288abc6b7c6d522be88aff7881cc6e082b7
Simon Jackson <simon@simonwjackson.com>
2026-03-11 23:37:11 -0600

    feat(aso): workstation setup without GUI

--- hosts/aso/default.nix
+++ hosts/aso/default.nix
@@ -14,9 +14,51 @@
   networking.hostName = "aso";
   time.timeZone = "America/Denver";
 
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
   users.users.simonwjackson = {
     isNormalUser = true;
-    extraGroups = ["wheel"];
+    extraGroups = ["wheel" "video"];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix = {
+    package = pkgs.nixVersions.latest;
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      trusted-users = ["root" "@wheel" "simonwjackson"];
+      trusted-substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+        "https://simonwjackson.cachix.org"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "simonwjackson.cachix.org-1:MtG0AE8J6bjFO/wD04X5h8MlQh7Sbee8KAJrAsPJydI="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+      auto-optimise-store = true;
+    };
   };
 
   # WSL2: netfilter not fully supported
