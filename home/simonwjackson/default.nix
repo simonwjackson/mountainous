d621f155068987b3abac0d1295bc3e27a9c6b263
Simon W. Jackson <github@simonwjackson.io>
2026-03-12 09:57:31 -0600

    feat(ssh): disable default config and add global defaults

--- home/simonwjackson/default.nix
+++ home/simonwjackson/default.nix
@@ -387,8 +387,21 @@ in {
 
     programs.ssh = {
       enable = true;
+      enableDefaultConfig = false;
       matchBlocks."aso" = {
         port = 8080;
+      };
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
       };
     };
 
