c22ee485ef419f38b8bb75fd2ad12badabc9510e
Simon W. Jackson <github@simonwjackson.io>
2026-03-13 03:24:22 +0000

    overlays: add steam-cage and steam-prefs packages

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,5 +4,7 @@
     gogcli = final.callPackage ../pkgs/gogcli.nix {};
     lifted-scripts = final.callPackage ../pkgs/scripts {};
     airconnect = final.callPackage ../pkgs/airconnect {};
+    steam-cage = final.callPackage ../nix/packages/steam-cage {};
+    steam-prefs = final.callPackage ../nix/packages/steam-prefs {};
   })
 ]
