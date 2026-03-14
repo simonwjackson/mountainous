8b949eb15ad0ec8db8f7d283dc287aca54ecadcf
Simon W. Jackson <github@simonwjackson.io>
2026-03-14 03:09:44 +0000

    refactor(secrets): register usenet service secrets for yari, update rakku key

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,15 +1,16 @@
 let
   fuji = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMKdNZb6qncSHcALFxtzADCDSEt+03VLhRQmNSn+rHa2";
   yari = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIvcfPMD8iAvxwWA8Epfk95CvszjzsQSDpetkjgdPZPh";
-  rakku = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCzfq63PfBDlmLY3JhfA/F6P+PDzgqhJUo0loDFm64niqjulExRp3kP9OIae0LZJ/Su4xN1vEOgPBEgJRvvUB7X1QGrndOqdJ1u71bcUYAl40vJXOX3OiprZ+rSX5pAhIYWWqun5o2VRGBQeeV1Ieco5kyMxLcfKNhqg9dqGViV/f0+vuBPgy8oqOeEu2/26XBqLNAjHO75XkjwGsEtJfbnaJq5qKIEPbLY3uLguIaWetPWY1gvNNOfawwZEH8NoSD1c/1cL8AoP9nhtiIbWh73MJboM9H/IJ/2z578R1yZgED0gF0cXMVztz6CO+1NoskZjau6QJPf4mYel9mpFXIOxIcaBha7+eB1dtYm5Wvsz//B4YR3N/BhyXYJzckAA7Ais5/ZC0EoI3CQCFyi1waY4LyCTrn1ayFgWqYou0HdEM1gYIfeVuopbhbTtIF0lIQgL6sjPoMWEW0+ThQ4Uja3twGCTSXv4s8mQuh+pkd0B+2bfiTeXy/HUJQK37ZQWRnUw1XXkti2Y3BH5Ru47Bq8NfYLnauDOV78mf0VgCnfpHm5Nihl/X7rX+AZoU5O8N/60vCxqqTA2vKUPLt3e9wjpZ5zLVl4WuxgfjJ1aF+Og0RQ2sV93dZkC94+HzI5zfJLpR3RtVdK6XGmz64iu8nXL1wlrp+adXi5m6LVKU2ASQ==";
+  rakku = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC8OeJfG7CpfHg7yrCOz8N/hwn7tyxYmmM3EGNDF8lwc3YXGnE7nm8kS/5csuaG4LQNbS5bXGSE30X2X78xGvgob/l0r2LvjHKMmEuDTQlXZdaVP8raBqI8/5DNwkpR91eqpV+2xXdr/LeDzLS/y/jLgKfhmNCyiXiKSsd67VykZuEEurMWVvZpEKS1dtHZliDULUPpG6qi7fu5qPTKm4msh88XCz44A8CMUrEUMkFadGyQ4SyyMZxnpRidn4NqSVbTosJmR96mXFvblB6tvZut25qnHlcmrtKknMecfRkyTgHoDegpDMUMjKd0LMhdXQ+IDwkVSdhs0F0vlnatxZyivb5j+OonZ/E6LcaOKtsww2Lo8/4p8plkewOvJ/yUBjSRgLDcz9M67O6rYdTVPZ0wdeiut4Ah3Vrh93FVv/88siDUyL3Zyiwm9p2bloU5rxmSIM+x4Kr0BAnZvoQrHdnbzHh/3M2rOmbL7EUVu2hJaN7UVAIZageGrElbxUlObMXWU0GqeWggUNehiMPvrv6DFYPcTPhSETwGbHdf8Dsk0QJQLlHccWhRPW2FV7TCxhH7oaFNEBSxtIqsq/HyC5pS2LdZslhP31EWlyyT/uXgpUe4eSmeAUI5NYoHEeV7KEW6OBSpbvk02ns0vf5lx4KSOpQL143IoZ2FC+MWRBfdyQ==";
   yuki = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFi4kQTMhafaVN7BrLiS7P7gS0v/VPAXeMWX0geqB0bj root@yuki";
   aso = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB4uSTAyOR0h8hrNltBDAQ9UnszVBZ9IVzyoA19r3uet root@nixos";
-  simonwjackson = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/PwyhdbVKd6jcG55m/1sUgEf0x3LUeS9H4EK5vk9PKhvDsjOQOISyR1LBmmXUFamkpFo2c84ZgPMj33qaPfOF0VfmF79vdAIDdDt5bmsTU6IbT7tGJ1ocpHDqhqbDO3693RdbTt1jTQN/eo3AKOfnrMouwBZPbPVqoWEhrLUvUTuTq7VQ+lUqWkvGs4D6D8UeIlG9VVgVhad3gCohYsjGdzgOUy0V4c8t3BuHrIE6//+6YVJ9VWK/ImSWmN8it5RIREDgdSYujs1Uod+ovr8AvaGFlFC9GuYMsj7xDYL1TgaWhy5ojk6JcuuF0cmoqffoW/apYdYM6Vxi5Xe6aJUhVyguZDovWcqRdPv2q0xtZn6xvNkoElEkrb6t0CAbGKf++H4h8/v5MsMt9wUPJAJBa24v0MlU8mXTUwhFLP5YQ/A8AAb5Y3ty/6DaOlvvTzt5Om2SMrZ1XaL1II35dFNZ/Os3zRpqdWq9SnpisRA+Bpf0bPUjdi8D8rRJn8g3zO5EsldBlZg82PiJcRHANbydTSK6Jzw7A8S5gMyPoH80Pq5MbQPvPpevTfOKy14NyTYPHGj0j5y7EQP7yb6w70LtqdRLRLQSTCdF0qTjTWw/qdt9MXkS7cdQe4yBADmjwozwPuxAs/jNpxELcVPEWBK6DcAIFD0vv3Xaw7reXpXFTQ==";
+  simonwjackson = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/PwyhdbVKd6jcG55m/1sUgEf0x3LUeS9H4EK5vk9PKhvDsjOQOISyR1LBmmXUFamkpFo2c84ZgPMj33qaPfOF0VfmF79vdAIDdDt5bmsTU6IbT7tGJ1ocpHDqhqbDO3693RdbTt1jTQN/eo3AKOfnrMouwBZPbPVqoWEhrLUvUTuTq7VQ+lUqWkvGs4D6D8UeIlG9VVgVhad3gCohYsjGdzgOUy0V4c8t3BuHrIE6//+6YVJ9VWK/ImSWmN8it5RIREDgdSYujs1Uod+ovr8AvaGFlFC9GuYMsj7xDYL1TgaWhy5ojk6JcuuF0cmoqffoW/apYdYM6Vxi5Xe6aJUhVyguZDovWcqRdPv2q0xtZn6xvNkoElEkrb6t0CAbGKf++H4h8/v5MsMt9wUPJAJBa24v0MlU8mXTUwhFLP5YQ/A8AAb5Y3ty/6DaOlvvTzt5Om2SMrZ1XaL1II35dFNZ/Os3zRpqdWq9SnpisRA+Bpf0bPUjdi8D8rRJn8g3zO5EsldBlZg82PiJcRHANbydTSK6Jzw7A8S5gMyPoH80Pq5MbQPvPpevTfOKy14NyTYPHGj0j5y7EQP7yb6w70LtqdRLRLQSTCdF0qTjVWw/qdt9MXkS7cdQe4yBADmjwozwPuxAs/jNpxELcVPEWBK6DcAIFD0vv3Xaw7reXpXFTQ==";
 
   fujiKeys = [fuji simonwjackson];
   yariKeys = [yari simonwjackson];
   fujiYariKeys = [fuji yari simonwjackson];
   rakkuKeys = [rakku simonwjackson];
+  rakkuYariKeys = [rakku yari simonwjackson];
   yukiKeys = [yuki simonwjackson];
   asoKeys = [aso simonwjackson];
   allKeys = [fuji yari rakku yuki aso simonwjackson];
@@ -42,8 +43,13 @@ in {
   "secrets/telegram-bot-token.age".publicKeys = fujiKeys;
   "secrets/ado-refresh-token.age".publicKeys = fujiKeys;
 
+  # Rakku / Yari shared service secrets
+  "secrets/tailscale-ephemeral.age".publicKeys = rakkuYariKeys;
+  "secrets/system/usenet/newsdemon-user.age".publicKeys = yariKeys;
+  "secrets/system/usenet/newsdemon-pass.age".publicKeys = yariKeys;
+  "secrets/system/usenet/nzbget-pass.age".publicKeys = yariKeys;
+
   # Rakku secrets
-  "secrets/tailscale-ephemeral.age".publicKeys = rakkuKeys;
   "secrets/zwave-js-secrets.age".publicKeys = rakkuKeys;
 
   # Yuki secrets
