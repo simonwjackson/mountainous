75d7ec0cb272071a62312e06fffdd6e8094142d7
Simon W. Jackson <github@simonwjackson.io>
2026-03-15 04:50:49 +0000

    feat(yuki): add kanata keyboard remaps

--- hosts/yuki/default.nix
+++ hosts/yuki/default.nix
@@ -67,6 +67,41 @@
       }
     }
   '';
+
+  yukiKanataConfig = pkgs.writeText "yuki-kanata.kbd" ''
+    (defcfg
+      process-unmapped-keys yes
+      concurrent-tap-hold yes
+      linux-continue-if-no-devs-found yes
+    )
+
+    (defsrc
+      a s h j k l u i o m comm . left down up right esc ret spc
+    )
+
+    (defalias
+      a-arr (tap-hold 200 200 a (layer-while-held arrows))
+      s-pad (tap-hold 200 200 s (layer-while-held numpad))
+      spc-met (tap-hold 200 200 spc lmet)
+    )
+
+    (defchordsv2
+      (j k) (macro ret) 150 first-release (arrows)
+      (k l) (macro esc) 150 first-release (arrows)
+    )
+
+    (deflayer base
+      @a-arr @s-pad h j k l u i o m comm . XX XX XX XX XX XX @spc-met
+    )
+
+    (deflayer arrows
+      a s left down up right u i o m comm . XX XX XX XX XX XX @spc-met
+    )
+
+    (deflayer numpad
+      a s h kp4 kp5 kp6 kp7 kp8 kp9 kp1 kp2 kp3 XX XX XX XX XX XX kp0
+    )
+  '';
 in {
   imports = [
     ./hardware.nix
@@ -148,9 +183,19 @@ in {
 
   environment.systemPackages = with pkgs; [
     acpi
+    kanata
   ];
 
   networking.networkmanager.wifi.powersave = lib.mkDefault true;
+
+  services.udev.extraRules = ''
+    # Ignore only the ThinkPad Bluetooth keyboard's integrated touchpad.
+    # Leave yuki's built-in touchpad, keyboard, pen, and touchscreen alone.
+    ACTION=="add|change", SUBSYSTEM=="input", ATTRS{name}=="ThinkPad Bluetooth TrackPoint Keyboard Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+
+    # Keep /dev/uinput available for the host-level Kanata remapper.
+    KERNEL=="uinput", MODE="0660", GROUP="input", OPTIONS+="static_node=uinput"
+  '';
 
   services = {
     geoclue2 = {
@@ -218,6 +263,7 @@ in {
   boot = {
     # Arrow Lake-H suspend-to-idle support requires Linux 6.15 or newer.
     kernelPackages = pkgs.linuxPackages_latest;
+    kernelModules = ["uinput"];
   };
 
   swapDevices = [
@@ -226,6 +272,27 @@ in {
       size = 20480;
     }
   ];
+
+  systemd.services.kanata = {
+    description = "Kanata keyboard remapper";
+    wantedBy = ["multi-user.target"];
+    after = ["systemd-udevd.service"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.kanata}/bin/kanata --cfg ${yukiKanataConfig}";
+      Restart = "always";
+      RestartSec = "3";
+      User = "root";
+      Group = "root";
+      PrivateTmp = true;
+      ProtectHome = true;
+      ProtectSystem = "strict";
+      DeviceAllow = [
+        "/dev/uinput rw"
+        "char-input r"
+      ];
+    };
+  };
 
   systemd.sleep.extraConfig = ''
     HibernateDelaySec=15min
