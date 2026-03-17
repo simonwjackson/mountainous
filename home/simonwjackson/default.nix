f09965bca4ba36993ca910d77d44bfe1d73729cd
Simon W. Jackson <github@simonwjackson.io>
2026-03-17 19:27:34 +0000

    feat(home): add usu ssh client defaults

--- home/simonwjackson/default.nix
+++ home/simonwjackson/default.nix
@@ -119,7 +119,12 @@
       matchBlocks."aso" = {
         port = 8080;
       };
+      matchBlocks."usu" = {
+        user = "nix-on-droid";
+        port = 2345;
+      };
       matchBlocks."*" = {
+        user = "simonwjackson";
         forwardAgent = false;
         addKeysToAgent = "no";
         compression = false;
