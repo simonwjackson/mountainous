42ad1d7af06f1fe23e05ab2b6f0a2cc02ccb5f86
Simon W. Jackson <github@simonwjackson.io>
2026-03-18 17:59:43 +0000

    refactor(usu): consolidate mountainous config into nested structure
    Reorganize usu host configuration to use a single mountainous object
    with presets and features sub-objects.

--- hosts/usu/default.nix
+++ hosts/usu/default.nix
@@ -1,22 +1,26 @@
 {
-  mountainous.presets.core.enable = true;
+  mountainous = {
+    presets.core.enable = true;
 
-  mountainous.features.atuin.enable = true;
-  mountainous.features.starship = {
-    enable = true;
-    hostName = "usu";
-  };
+    features = {
+      atuin.enable = true;
+      starship = {
+        enable = true;
+        hostName = "usu";
+      };
 
-  mountainous.features.ssh.server = {
-    enable = true;
-    port = 2345;
-    mosh.enable = true;
-  };
+      ssh.server = {
+        enable = true;
+        port = 2345;
+        mosh.enable = true;
+      };
 
-  mountainous.features.secrets = {
-    enable = true;
-    hostname = "usu";
-    identityFile = "/data/data/com.termux.nix/files/home/.ssh/id_rsa";
-    secretsDir = "/data/data/com.termux.nix/files/home/.secrets";
+      secrets = {
+        enable = true;
+        hostname = "usu";
+        identityFile = "/data/data/com.termux.nix/files/home/.ssh/id_rsa";
+        secretsDir = "/data/data/com.termux.nix/files/home/.secrets";
+      };
+    };
   };
 }
