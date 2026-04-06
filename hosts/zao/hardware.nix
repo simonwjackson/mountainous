bca299352f04f018f5fa03644000aeae25ce61ac
Simon W. Jackson <github@simonwjackson.io>
2026-04-06 00:51:19 +0000

    feat(zao/hardware): add hardware configuration with NVIDIA GPU and security settings

--- hosts/zao/hardware.nix
+++ hosts/zao/hardware.nix
@@ -1,4 +1,4 @@
-{modulesPath, ...}: {
+{lib, modulesPath, ...}: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
@@ -21,5 +21,29 @@
 
   nixpkgs.hostPlatform = "x86_64-linux";
 
-  hardware.cpu.intel.updateMicrocode = true;
+  hardware = {
+    cpu.intel.updateMicrocode = true;
+    enableRedistributableFirmware = lib.mkDefault true;
+    i2c.enable = lib.mkDefault true;
+    nvidia = {
+      modesetting.enable = true;
+      open = false;
+      powerManagement.enable = lib.mkDefault true;
+      prime = {
+        offload.enable = true;
+        offload.enableOffloadCmd = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
+
+  security.tpm2.enable = lib.mkDefault true;
+
+  services = {
+    fwupd.enable = lib.mkDefault true;
+    thermald.enable = lib.mkDefault true;
+    hardware.bolt.enable = lib.mkDefault true;
+    xserver.videoDrivers = ["nvidia"];
+  };
 }
